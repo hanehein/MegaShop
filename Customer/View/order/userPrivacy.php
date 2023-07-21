a502e291559c533d4a4569a32d0e62cfa782dc42
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2023-07-22 00:58:34 +0630

    navbar

--- Customer/View/order/userPrivacy.php
+++ Customer/View/order/userPrivacy.php
@@ -1,12 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>privacy&policy</title>
     <link rel="stylesheet" href="../resources/lib/tailwind/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&family=Wallpoet&display=swap" rel="stylesheet">
 </head>
-<body class="bg-blue-600">
-    <p class="text-red-400">Hello</p>
+
+<body class="bg-[#F6F6F6] overflow-x-hidden relative">
+    <div class="w-full h-screen flex flex-col items-center">
+        <!-- navbar -->
+        <nav class="w-full h-14 bg-white flex justify-between items-center px-4 z-50 fixed">
+            <div class="font-['Wallpoet'] text-2xl text-[#003366] leading-none cursor-pointer">
+                <p>MEGA</p>
+                <p>SHOP</p>
+            </div>
+            <div class="w-auto px-2 py-2">
+                <ul class="uppercase text-[#003366] text-xs font-semibold flex items-center space-x-5 font-['Poppins'] cursor-pointer">
+                    <li class="hover:text-[#F68721]"><a href="">Home</a></li>
+                    <li class="hover:text-[#F68721]"><a href="">Products</a></li>
+                    <li class="hover:text-[#F68721]"><a href="">Shops</a></li>
+                    <li class="hover:text-[#F68721]"><a href="">About US</a></li>
+                    <li class="hover:text-[#F68721]"><a href="">Contact US</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center justify-center space-x-2 cursor-pointer">
+                <ion-icon name="cart" size="large" class="text-[#003366] hover:text-[#F68721] "></ion-icon>
+                <ion-icon name="ellipse" class="text-[#F68721] mb-6 hover:text-[#003366]"></ion-icon>
+                <ion-icon name="person-circle" size="large" class="text-[#003366] pl-4 hover:text-[#F68721]"></ion-icon>
+            </div>
+
+        </nav>
+    </div>
 </body>
+
 </html>
+<!-- blue 800
+    slate-100
+    orange-500
+     primary - #003366
+     secondary - #F68721
+     tertiary - #F6F6F6
+    <ion-icon name="person-circle"></ion-icon>
+    <ion-icon name="cart"></ion-icon>
+    <ion-icon name="ellipse"></ion-icon>
+    <ion-icon name="caret-down"></ion-icon>
+    <ion-icon name="logo-facebook"></ion-icon>
+    <ion-icon name="logo-instagram"></ion-icon>
+    <ion-icon name="logo-twitter"></ion-icon>
+    <ion-icon name="search"></ion-icon>
+    
+-->
