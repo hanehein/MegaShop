16557b7076751dd130939dca006c8b45c54d0b15
Drey <bhon3myatpaing@gmail.com>
2023-07-21 15:13:20 +0630

    test

--- Customer/View/Product/profile.php
+++ Customer/View/Product/profile.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="../resources/lib/tailwind/output.css">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 </head>
 
 <body class="bg-slate-100">
