2c74eee614cd882cfdebadcb557d5548dab6ec78
Hein Thu <88365143+hanehein@users.noreply.github.com>
2023-07-21 14:58:29 +0630

    Add files via upload

---
+++ Customer/View/Product/test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Item Detail</title>
+    <link rel="stylesheet" href="../resources/lib/tailwind/output.css">
+</head>
+
+<body class="text-lg h-screen bg-red-300">
+    <h1>Testing Page</h1>
+    <p>TEst 2</p>
+    <select name="" id="">
+        <option value="">1</option>
+        <option value="">2</option>
+    </select>
+    <p>Hello</p>
+    <p>test</p>
+</body>
+
+</html>
