79a194796f5f2ae2df1cc9c38eb1ff6d46eab6ef
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2023-07-22 23:44:52 +0630

    shoplist update

--- Customer/View/Product/shoplists.php
+++ Customer/View/Product/shoplists.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>privacy&policy</title>
+    <title>shoplists</title>
     <link rel="stylesheet" href="../resources/lib/tailwind/output.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
@@ -23,7 +23,7 @@
             </div>
             <div class="font-['Wallpoet'] text-2xl text-[#024486] leading-none cursor-pointer">
                 <p class="">MEGA</p>
-                <p >SHOP</p>
+                <p>SHOP</p>
             </div>
             <div class="md:w-auto hidden md:flex px-2 py-2">
                 <ul class="uppercase text-[#024486] text-xs font-semibold flex items-center space-x-5 font-['Poppins'] cursor-pointer">
@@ -42,21 +42,97 @@
                 <div class="cursor-pointer flex items-center justify-center">
                     <ion-icon name="person-circle" size="large" class="text-[#024486] hover:text-[#F68721]"></ion-icon>
                 </div>
-            </div> 
+            </div>
         </nav>
+        <!-- description -->
+        <div class="w-full flex justify-center items-center font-['Poppins'] text-md font-semibold text-[#024486] mt-5">
+            <p>Shop by Shop Lists and Brands</p>
+        </div>
         <!-- body box -->
-        <div class="w-full container mx-auto grid grid-cols-4 grid-rows-4 gap-2 bg-[#024486]">
-                <div class="w-40 h-12">
-                    
-                </div>
+        <div class="container mx-auto grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2 grid-rows-5 gap-5 gap-x-0 place-items-center bg-[#F6F6F6] mt-5 ">
+            <div class="w-40 h-20 bg-white rounded-sm shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/fortnite.jpg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/redfall.jpeg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer  flex items-center justify-center">
+                <img src="../resources/img/undraw_search_app_oso2.svg" alt="" class="rounded-md" width="50%">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/EGS_DeadbyDaylight.jpg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/genshin.jpg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/minecraft.jpg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/redfall.jpeg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/heroes-of-rome-3.jpg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/remains.jpg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/Honkai-Star-Rail.webp" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/sphere3.jpg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/world-turtle.jpg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/callfoduty.png" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 bg-white rounded-md shadow-md shadow-gray-600 hover:w-44 hover:cursor-pointer">
+                <img src="../resources/img/fortnite.jpg" alt="" class="rounded-md">
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  hover:cursor-pointer ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  hover:cursor-pointer ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  hover:cursor-pointer ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  hover:cursor-pointer ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  hover:cursor-pointer ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  hover:cursor-pointer ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  hover:cursor-pointer ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  hover:cursor-pointer ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
+            <div class="w-40 h-20 flex items-center justify-center bg-white rounded-md shadow-md shadow-gray-600  ">
+                <button class="rounded-md shadow-md shadow-gray-600 hover:bg-blue-800 hover:text-white w-28 text-xs text-blue-800 h-10 bg-slate-400 opacity-30 border-2   border-blue-800 font-semibold">Space Availabe</button>
+            </div>
         </div>
         <!-- footer responsive -->
-        <div class="w-full  flex flex-col items-center justify-center font-['Poppins'] bg-[#024486]">
+        <div class="w-full  flex flex-col items-center justify-center font-['Poppins'] bg-[#024486] mt-5">
             <!-- first one -->
             <div class="md:w-full w-[28rem] container md:flex mx-auto  items-center justify-between sm:justify-center mt-3 py-3 md:space-x-20">
                 <!-- quicklink -->
                 <div class=" md:w-50 w-[28rem] flex flex-col  items-center justify-center text-white text-xs px-3 py-2">
-                    <p >Quick Links</p>
+                    <p>Quick Links</p>
                     <ul class="list-disc mt-2">
                         <li>About Us</li>
                         <li>Pricacy & Policy</li>
@@ -64,7 +140,7 @@
                 </div>
                 <!-- Help -->
                 <div class=" md:w-50 w-[28rem] flex flex-col  items-center justify-center text-white text-xs px-3 py-2 md:border-hidden border-t-2  border-b-white">
-                    <p >Help?</p>
+                    <p>Help?</p>
                     <ul class="list-disc mt-2">
                         <li>Payment</li>
                         <li>FAQ</li>
@@ -72,7 +148,7 @@
                 </div>
                 <!-- Contact Us -->
                 <div class=" w-50 w-[28rem] flex flex-col items-center justify-center text-white text-xs px-3 py-2 md:border-hidden border-t-2 border-b-white ">
-                    <p >Contact Us</p>
+                    <p>Contact Us</p>
                     <ul class="list-disc mt-2 leading-5">
                         <li><ion-icon name="phone-portrait"></ion-icon><span class="ml-2">+95777777777</span></li>
                         <li><ion-icon name="mail"></ion-icon><span class="ml-2">service@gmail.com</span></li>
