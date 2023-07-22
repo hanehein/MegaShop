83576a776933549eb5c2f5f06a368321f1713941
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2023-07-22 20:31:33 +0630

    Update userPrivacy.php

--- Customer/View/order/userPrivacy.php
+++ Customer/View/order/userPrivacy.php
@@ -14,16 +14,19 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&family=Wallpoet&display=swap" rel="stylesheet">
 </head>
 
-<body class="bg-[#F6F6F6] overflow-x-hidden relative">
-    <div class="w-full h-screen flex flex-col items-center">
-        <!-- navbar -->
-        <nav class="w-full h-14 bg-white flex justify-between items-center px-4 z-50 fixed">
-            <div class="font-['Wallpoet'] text-2xl text-[#003366] leading-none cursor-pointer">
-                <p>MEGA</p>
-                <p>SHOP</p>
+<body class="bg-[#F6F6F6] overflow-x-hidden">
+    <div class="w-full h-screen  flex flex-col items-center">
+        <!-- navbar responsive -->
+        <nav class="w-full h-14 bg-white flex justify-between items-center px-4 -z-50">
+            <div class=" md:hidden">
+                <ion-icon name="menu" size="large" class="text-[#024486] hover:text-[#F68721]"></ion-icon>
             </div>
-            <div class="w-auto px-2 py-2">
-                <ul class="uppercase text-[#003366] text-xs font-semibold flex items-center space-x-5 font-['Poppins'] cursor-pointer">
+            <div class="font-['Wallpoet'] text-2xl text-[#024486] leading-none cursor-pointer">
+                <p class="">MEGA</p>
+                <p >SHOP</p>
+            </div>
+            <div class="md:w-auto hidden md:flex px-2 py-2">
+                <ul class="uppercase text-[#024486] text-xs font-semibold flex items-center space-x-5 font-['Poppins'] cursor-pointer">
                     <li class="hover:text-[#F68721]"><a href="">Home</a></li>
                     <li class="hover:text-[#F68721]"><a href="">Products</a></li>
                     <li class="hover:text-[#F68721]"><a href="">Shops</a></li>
@@ -31,13 +34,104 @@
                     <li class="hover:text-[#F68721]"><a href="">Contact US</a></li>
                 </ul>
             </div>
-            <div class="flex items-center justify-center space-x-2 cursor-pointer">
-                <ion-icon name="cart" size="large" class="text-[#003366] hover:text-[#F68721] "></ion-icon>
-                <ion-icon name="ellipse" class="text-[#F68721] mb-6 hover:text-[#003366]"></ion-icon>
-                <ion-icon name="person-circle" size="large" class="text-[#003366] pl-4 hover:text-[#F68721]"></ion-icon>
+            <div class="flex items-center justify-center space-x-3">
+                <div class="cursor-pointer flex items-center justify-center">
+                    <ion-icon name="cart" size="large" class="text-[#024486] hover:text-[#F68721] "></ion-icon>
+                    <ion-icon name="ellipse" class="text-[#F68721] mb-6 hover:text-[#024486]"></ion-icon>
+                </div>
+                <div class="cursor-pointer flex items-center justify-center">
+                    <ion-icon name="person-circle" size="large" class="text-[#024486] hover:text-[#F68721]"></ion-icon>
+                </div>
+            </div> 
+        </nav>
+        <!-- search box -->
+        <div class="w-full h-20 flex flex-col justify-center items-center bg-[#024486] font-['Poppins'] py-3">
+            <p class="text-white text-sm mb-2">How can we help?</p>
+            <div class="md:w-56 w-48 h-7 flex justify-between items-center bg-[#F6F6F6] rounded-md px-1 py-2 shadow-md shadow-black-500">
+                <input type="text" name="search" id="" class="md:w-48 w-40 text-sm outline-none bg-[#F6F6F6]" placeholder="Search here...">
+                <div class="bg-[#024486] w-7 h-5 mx-auto text-center rounded-md">
+                    <ion-icon name="search" class="text-white"></ion-icon>
+                </div>
+            </div>
+        </div>
+        <!-- body box -->
+        <div class="w-[27rem] h-[48rem] container flex flex-col bg-white justify-center items-center my-8 font-['Poppins'] rounded-md shadow-md py-6">
+            <!-- policies -->
+            <div class="w-32 h-8 flex items-center justify-center bg-[#F68721] rounded-md px-2 shadow-md shadow-slate-500">
+                <p class="text-white text-sm">General Policies</p>
+            </div>
+            <div class=" w-96 h-10 flex justify-between items-center text-gray-700 text-xs space-x-5 mt-8 border-t-2 border-b-2  border-t-blue-700 border-b-blue-800">
+                <p>Terms of Services</p>
+                <ion-icon name="caret-down" class="text-[#F68721]" size="small"></ion-icon>
+            </div>
+            <div class=" w-96 h-10 flex justify-between items-center text-gray-700 text-xs space-x-5   border-b-2 border-b-blue-800">
+                <p>Terms of Services</p>
+                <ion-icon name="caret-down" class="text-[#F68721]" size="small"></ion-icon>
+            </div>
+            <div class=" w-96 h-10 flex justify-between items-center text-gray-700 text-xs space-x-5   border-b-2 border-b-blue-800">
+                <p>Terms of Services</p>
+                <ion-icon name="caret-down" class="text-[#F68721]" size="small"></ion-icon>
+            </div>
+            <div class=" w-96 h-10 flex justify-between items-center text-gray-700 text-xs space-x-5   border-b-2 border-b-blue-800">
+                <p>Terms of Services</p>
+                <ion-icon name="caret-down" class="text-[#F68721]" size="small"></ion-icon>
             </div>
 
-        </nav>
+            <!-- rule -->
+            <div class="w-32 h-8 flex items-center justify-center bg-[#F68721] rounded-md px-2 mt-8 shadow-md shadow-slate-500">
+                <p class="text-white text-sm">Rule&Regulation</p>
+            </div>
+            <div class=" w-96 h-10 flex justify-between items-center text-gray-700 text-xs space-x-5 mt-8 border-t-2 border-b-2  border-t-blue-700 border-b-blue-800">
+                <p>Terms of Services</p>
+                <ion-icon name="caret-down" class="text-[#F68721]" size="small"></ion-icon>
+            </div>
+            <div class=" w-96 h-10 flex justify-between items-center text-gray-700 text-xs space-x-5   border-b-2 border-b-blue-800">
+                <p>Terms of Services</p>
+                <ion-icon name="caret-down" class="text-[#F68721]" size="small"></ion-icon>
+            </div>
+            <div class=" w-96 h-10 flex justify-between items-center text-gray-700 text-xs space-x-5   border-b-2 border-b-blue-800">
+                <p>Terms of Services</p>
+                <ion-icon name="caret-down" class="text-[#F68721]" size="small"></ion-icon>
+            </div>
+            <div class=" w-96 h-10 flex justify-between items-center text-gray-700 text-sm space-x-5   border-b-2 border-b-blue-800">
+                <p>Terms of Services</p>
+                <ion-icon name="caret-down" class="text-[#F68721]" size="small"></ion-icon>
+            </div>
+        </div>
+        <!-- footer responsive -->
+        <div class="w-full  flex flex-col items-center justify-center font-['Poppins'] bg-[#024486]">
+            <!-- first one -->
+            <div class="md:w-full w-[28rem] container md:flex mx-auto  items-center justify-between sm:justify-center mt-3 py-3 md:space-x-20">
+                <!-- quicklink -->
+                <div class=" md:w-50 w-[28rem] flex flex-col  items-center justify-center text-white text-xs px-3 py-2">
+                    <p >Quick Links</p>
+                    <ul class="list-disc mt-2">
+                        <li>About Us</li>
+                        <li>Pricacy & Policy</li>
+                    </ul>
+                </div>
+                <!-- Help -->
+                <div class=" md:w-50 w-[28rem] flex flex-col  items-center justify-center text-white text-xs px-3 py-2 md:border-hidden border-t-2  border-b-white">
+                    <p >Help?</p>
+                    <ul class="list-disc mt-2">
+                        <li>Payment</li>
+                        <li>FAQ</li>
+                    </ul>
+                </div>
+                <!-- Contact Us -->
+                <div class=" w-50 w-[28rem] flex flex-col items-center justify-center text-white text-xs px-3 py-2 md:border-hidden border-t-2 border-b-white ">
+                    <p >Contact Us</p>
+                    <ul class="list-disc mt-2 leading-5">
+                        <li><ion-icon name="phone-portrait"></ion-icon><span class="ml-2">+95777777777</span></li>
+                        <li><ion-icon name="mail"></ion-icon><span class="ml-2">service@gmail.com</span></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- last one -->
+            <div class="md:w-[80rem] w-[28rem] flex items-center justify-center border-t-2 border-b-white mt-1 py-3">
+                <p class="text-white text-xs">&#64;2023 MEGA SHOP All Rights Reserved.</p>
+            </div>
+        </div>
     </div>
 </body>
 
@@ -45,7 +139,7 @@
 <!-- blue 800
     slate-100
     orange-500
-     primary - #003366
+     primary - #024486
      secondary - #F68721
      tertiary - #F6F6F6
     <ion-icon name="person-circle"></ion-icon>
@@ -56,5 +150,6 @@
     <ion-icon name="logo-instagram"></ion-icon>
     <ion-icon name="logo-twitter"></ion-icon>
     <ion-icon name="search"></ion-icon>
-    
+    <ion-icon name="phone-portrait"></ion-icon>
+    <ion-icon name="mail"></ion-icon>
 -->
