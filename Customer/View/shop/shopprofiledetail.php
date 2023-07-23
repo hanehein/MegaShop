050eb45f8d80ae400d22d22e3618e12b339aabcc
DESKTOP-GM29KLB\HP <myatnoe.khin.9440@gmail.com>
2023-07-23 14:51:14 +0630

    shop detail

--- Customer/View/shop/shopprofiledetail.php
+++ Customer/View/shop/shopprofiledetail.php
@@ -58,16 +58,16 @@
             </ul>
         </div>
     </div>
-    <div class="flex flex-row bg-white mt-3 w-[500px] p-3 container mx-3 justify-between">
-        <div class="flex flex-row space-x-4">
+    <div class="flex flex-row bg-white mt-3 w-[350px] md:w-[500px] p-3 container mx-3 justify-between">
+        <div class="flex flex-row space-x-2 md:space-x-4">
             <img src="../resources/images/shopprofileimages/flower.jpg" alt="" class="w-[50px] h-[50px]">
 
             <div class="flex flex-col">
-                <p class="text-blue-800 text-2xl font-semibold">Trendy Thread</p>
+                <p class="text-blue-800 text-xl md:text-2xl font-semibold">Trendy Thread</p>
                 <p>235 followers</p>
             </div>
         </div>
-        <div class="flex flex-row space-x-8">
+        <div class="flex flex-row space-x-2 md:space-x-8">
             <div class="flex flex-col text-blue-800 justify-center items-center font-semibold">
                 <ion-icon name="chatbox-ellipses-outline"></ion-icon>
                 <p>Chat Now</p>
@@ -78,8 +78,9 @@
             </div>
         </div>
     </div>
-    <div class="container mx-3 bg-white flex flex-row justify-between items-center mt-3 p-3 font-semibold text-lg">
-        <div class="flex flex-row space-x-5">
+    <div class="container mx-3 bg-white hidden md:flex flex-row justify-between items-center mt-3 p-2 md:p-3 font-semibold text-xs md:text-lg">
+        <div class="flex flex-row space-x-2 md:space-x-5">
+            <p>Homepage</p>
             <p>All products</p>
             <span>Profile</span>
         </div>
@@ -89,10 +90,10 @@
             <span class="bg-blue-800 p-2 text-white rounded-r-md">Search</span>
         </div>
     </div>
-    <section class="bg-white mt-3 space-y-6">
-        <p class="text-lg font-bold ml-[535px]">Average Seller Ratings</p>
+    <section class="bg-white mt-3 space-y-6 p-5">
+        <p class="text-lg font-bold ml-[80px] md:ml-[535px]">Average Seller Ratings</p>
         <div class="flex flex-col justify-between items-center space-y-3">
-            <div class="flex flex-row items-center space-x-11">
+            <div class="flex flex-col md:flex-row space-y-5 md:space-y-0 items-center space-x-11">
                 <div class="flex flex-col space-y-2 items-start">
                     <h1 class="text-xl font-semibold">94%</h1>
                     <div class="flex flex-row items-center justify-center space-x-3">
@@ -119,6 +120,10 @@
                 </div>
                 <div class="flex flex-col space-y-2 items-center">
                     <h1 class="font-semibold text-xl">Seller Ratings and Reviews(16)</h1>
+                    <div class="flex space-x-16">
+                        <ion-icon name="happy-outline"></ion-icon>
+                        <ion-icon name="happy-outline"></ion-icon>
+                    </div>
                     <div class="flex flex-row space-x-5">
                         <p>Positive</p>
                         <p>Natural</p>
@@ -126,7 +131,7 @@
                     </div>
                 </div>
             </div>
-            <div class="border-4 border-blue-200 w-[800px] h-[180px] rounded-md p-3">
+            <div class="border-4 border-blue-200 w-[350px] h[300px] md:w-[800px] md:h-[180px] rounded-md p-3">
                 <div class="flex flex-col space-y-2">
                     <div class="flex justify-between items-center">
                         <div class="flex space-x-2 items-center">
@@ -136,7 +141,7 @@
                         <p class="text-grey">27 Oct 2022</p>
                     </div>
                     <div class="flex items-center space-x-1">
-                        <ion-icon name="happy-outline" class="text-xl"></ion-icon>
+                        <ion-icon name="happy" class="text-xl text-yellow-600"></ion-icon>
                         <span>Positive</span>
                     </div>
                     <p>I recently purchased the [Brand Name] backpack, and I must say, it has exceeded my expectations in every way.
@@ -144,9 +149,115 @@
                         commutes to weekend getaways. Here are my thoughts on this fantastic product.</p>
                 </div>
             </div>
+            <div class="border-4 border-blue-200 w-[350px] h[300px] md:w-[800px] md:h-[180px] rounded-md p-3">
+                <div class="flex flex-col space-y-2">
+                    <div class="flex justify-between items-center">
+                        <div class="flex space-x-2 items-center">
+                            <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80" class="w-[40px] h-[40px] rounded-full" alt="user-profile" />
+                            <span class="text-grey text-sm font-bold">Sandy</span>
+                        </div>
+                        <p class="text-grey font-light">27 Oct 2022</p>
+                    </div>
+                    <div class="flex items-center space-x-1">
+                        <ion-icon name="happy" class="text-xl text-yellow-600"></ion-icon>
+                        <span>Positive</span>
+                    </div>
+                    <p>from daily commutes to weekend getaways. Here are my thoughts on this fantastic product.</p>
+                </div>
+            </div>
+            <div class="border-4 border-blue-200 w-[350px] h[300px] md:w-[800px] md:h-[180px] rounded-md p-3">
+                <div class="flex flex-col space-y-2">
+                    <div class="flex justify-between items-center">
+                        <div class="flex space-x-2 items-center">
+                            <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80" class="w-[40px] h-[40px] rounded-full" alt="user-profile" />
+                            <span class="text-grey text-sm font-bold">Sandy</span>
+                        </div>
+                        <p class="text-grey">27 Oct 2022</p>
+                    </div>
+                    <div class="flex items-center space-x-1">
+                        <ion-icon name="happy" class="text-xl text-yellow-600"></ion-icon>
+                        <span>Positive</span>
+                    </div>
+                    <p>from daily commutes to weekend getaways. Here are my thoughts on this fantastic product.</p>
+                </div>
+            </div>
+            <div class="flex justify-center items-center">
+                <div class="w-[30px] h-[30px] md:w-[50px] md:h-[50px] border-2 border-blue-600 flex justify-center items-center rounded-l-md"></div>
+                <div class="w-[30px] h-[30px] md:w-[50px] md:h-[50px] border-2 border-blue-600 flex justify-center items-center bg-blue-600">1</div>
+                <div class="w-[30px] h-[30px] md:w-[50px] md:h-[50px] border-2 border-blue-600 flex justify-center items-center">2</div>
+                <div class="w-[30px] h-[30px] md:w-[50px] md:h-[50px] border-2 border-blue-600 flex justify-center items-center">3</div>
+                <div class="w-[30px] h-[30px] md:w-[50px] md:h-[50px] border-2 border-blue-600 flex justify-center items-center">...</div>
+                <div class="w-[30px] h-[30px] md:w-[50px] md:h-[50px] border-2 border-blue-600 flex justify-center items-center">41</div>
+                <div class="w-[30px] h-[30px] md:w-[50px] md:h-[50px] border-2 border-blue-600 flex justify-center items-center">42</div>
+                <div class="w-[30px] h-[30px] md:w-[50px] md:h-[50px] border-2 border-blue-600 flex justify-center items-center">43</div>
+                <div class="w-[30px] h-[30px] md:w-[50px] md:h-[50px] border-2 border-blue-600 flex justify-center items-center rounded-r-md"></div>
+            </div>
         </div>
     </section>
-
+    <section class="bg-white flex flex-col p-5 mt-5">
+        <p class="text-xl font-semibold">Product Ratings & Reviews(345)</p>
+        <div class="flex space-x-6 p-5">
+            <img src="../resources/img/cola.jpg" alt="" class="w-[80px] h-[120px]">
+            <div class="flex flex-col">
+                <p class="text-blue-600 font-bold text-2xl mb-3">STEP Women Blouse Short Sleeves 007402</p>
+                <div class="flex space-x-1">
+                    <ion-icon name="star" class="text-yellow-700"></ion-icon>
+                    <ion-icon name="star" class="text-yellow-700"></ion-icon>
+                    <ion-icon name="star" class="text-yellow-700"></ion-icon>
+                    <ion-icon name="star" class="text-yellow-700"></ion-icon>
+                    <ion-icon name="star" class="text-yellow-700"></ion-icon>
+                </div>
+                <p>I recently purchased the [Brand Name] backpack, and I must say, it has exceeded my expectations in every way.
+                    This backpack has proven to be a reliable companion, accompanying me on various adventures, from daily
+                    commutes to weekend getaways. Here are my thoughts on this fantastic product.</p>
+                <div class="flex space-x-4 mt-5">
+                    <img src="../resources/img/colacan.jpg" alt="" class="w-[100px] h-[130px]">
+                    <img src="../resources/img/oreo.jpg" alt="" class="w-[100px] h-[130px]">
+                </div>
+                <div class="flex space-x-3">
+                    <p class="text-gray-600 font-thin">27 Oct 2022 By</p>
+                    <p class="text-blue-600 font-semibold">Mark Albert</p>
+                </div>
+            </div>
+        </div>
+        <hr class="w-full px-5 h-5 text-black font-bold text-center mt-4" />
+        <div class="flex space-x-6 p-5">
+            <img src="../resources/img/cola.jpg" alt="" class="w-[80px] h-[120px]">
+            <div class="flex flex-col">
+                <p class="text-blue-600 font-bold text-2xl mb-3">STEP Women Blouse Short Sleeves 007402</p>
+                <div class="flex space-x-1">
+                    <ion-icon name="star" class="text-yellow-700"></ion-icon>
+                    <ion-icon name="star" class="text-yellow-700"></ion-icon>
+                    <ion-icon name="star" class="text-yellow-700"></ion-icon>
+                    <ion-icon name="star" class="text-yellow-700"></ion-icon>
+                    <ion-icon name="star" class="text-yellow-700"></ion-icon>
+                </div>
+                <p>I recently purchased the [Brand Name] backpack, and I must say, it has exceeded my expectations in every way.
+                    This backpack has proven to be a reliable companion, accompanying me on various adventures, from daily
+                    commutes to weekend getaways. Here are my thoughts on this fantastic product.</p>
+                <div class="flex space-x-4 mt-5">
+                    <img src="../resources/img/colacan.jpg" alt="" class="w-[100px] h-[130px]">
+                    <img src="../resources/img/oreo.jpg" alt="" class="w-[100px] h-[130px]">
+                </div>
+                <div class="flex space-x-3">
+                    <p class="text-gray-600 font-thin">27 Oct 2022 By</p>
+                    <p class="text-blue-600 font-semibold">Mark Albert</p>
+                </div>
+            </div>
+        </div>
+        <hr class="w-full px-5 h-5 text-black font-bold text-center mt-4" />
+        <div class="flex justify-center items-center">
+            <div class="w-[50px] h-[50px] border-2 border-blue-600 flex justify-center items-center rounded-l-md"></div>
+            <div class="w-[50px] h-[50px] border-2 border-blue-600 flex justify-center items-center bg-blue-600">1</div>
+            <div class="w-[50px] h-[50px] border-2 border-blue-600 flex justify-center items-center">2</div>
+            <div class="w-[50px] h-[50px] border-2 border-blue-600 flex justify-center items-center">3</div>
+            <div class="w-[50px] h-[50px] border-2 border-blue-600 flex justify-center items-center">...</div>
+            <div class="w-[50px] h-[50px] border-2 border-blue-600 flex justify-center items-center">410</div>
+            <div class="w-[50px] h-[50px] border-2 border-blue-600 flex justify-center items-center">411</div>
+            <div class="w-[50px] h-[50px] border-2 border-blue-600 flex justify-center items-center">412</div>
+            <div class="w-[50px] h-[50px] border-2 border-blue-600 flex justify-center items-center rounded-r-md"></div>
+        </div>
+    </section>
 </body>
 
 </html>
