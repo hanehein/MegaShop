62e47c277341af91c811a303168284e5bcdf3027
Maung Thar Kyaw <maungtharkyaw2520@gmail.com>
2023-07-23 21:18:50 +0630

    adding shoppingCart.php file and adding custom data to config.js

---
+++ Customer/View/order/shoppingCart.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>privacy&policy</title>
+    <link rel="stylesheet" href="../resources/lib/tailwind/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&family=Wallpoet&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!-- START NAVBAR -->
+    <nav class="px-4 sm:px-[0px] ">
+        <div class="container mx-auto flex justify-between py-4">
+            <div>
+                <a href="" class="text-2xl font-bold">MEGA SHOP</a>
+            </div>
+            <div class="hidden sm:block">
+                <ul class="flex space-x-6 text-custom-blue font-bold">
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Shop Lists</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div class="hidden sm:block">
+                <button class="px-6 py-1 text-white bg-blue-800 rounded-2xl"><a href="">log in</a></button>
+                <button class="px-6 py-1 text-white bg-blue-800 rounded-2xl"><a href="">sign up</a></button>
+            </div>
+            <div class="block sm:hidden">
+                <ion-icon class="text-3xl" name="menu"></ion-icon>
+            </div>
+        </div>
+    </nav>
+    <!-- END NAVBAR -->
+
+    <!-- START Shopping Cart SECTION -->
+    <section class="p-4">
+        <div class="container mx-auto block sm:grid sm:grid-cols-3 sm:gap-4">
+            <div class="card-container sm:col-span-2 mb-4">
+
+                <!-- start cart -->
+                <div class="rounded-md overflow-hidden shadow-lg">
+                    <div class="p-2 bg-custom-blue">
+                        <p class="text-white"><a href="">Traffic Star</a></p>
+                    </div>
+                    <div class="flex justify-between p-4 items-center">
+                        <div class="block sm:hidden">
+                            <img class="max-h-[130px]" src="https://cdn.ospreyeurope.com/shop/media/catalog/product/cache/90fbb2341f4f0519805a950f9933da0d/_/0/_0035_ozonelaptopbackpack28_f22_side_coastalblue_10004643.jpg" alt="product-img">
+                        </div>
+                        <div class="block sm:flex sm:justify-between sm:w-full sm:items-center">
+                            <div class="mb-2 flex items-center">
+                                <div class="hidden sm:block">
+                                    <img class="max-h-[100px]" src="https://cdn.ospreyeurope.com/shop/media/catalog/product/cache/90fbb2341f4f0519805a950f9933da0d/_/0/_0035_ozonelaptopbackpack28_f22_side_coastalblue_10004643.jpg" alt="product-img">
+                                </div>
+                                <div>
+                                    <h2 class="text-custom-large font-bold">Tucano Backpacks TU-BP01</h2>
+                                    <p class="text-custom-grey">Color : blue</p>
+                                    <p class="text-custom-grey">Size : M</p>
+                                    <div class="space-x-2 hidden sm:flex">
+                                        <div class="items-center">
+                                            <ion-icon class="text-custom-medium" name="trash-bin"></ion-icon>
+                                            <span class="text-custom-tiny text-custom-grey"> Remove</span>
+                                        </div>
+                                        <div class="items-center">
+                                            <ion-icon class="text-custom-medium" name="heart-outline"></ion-icon>
+                                            <span class="text-custom-tiny text-custom-grey"> Add To Wishlists</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex space-x-2 sm:flex-col items-center mb-2">
+                                <p class="text-custom-orange text-custom-extra-large">Ks 23450</p>
+                                <p class="text-custom-tiny line-through">Ks 33500</p>
+                                <p class="text-custom-tiny text-custom-orange">(30%)</p>
+                            </div>
+                            <div>
+                                <div class="items-center">
+                                    <div class="flex space-x-2 items-center">
+                                        <div class="w-[30px] h-[30px] rounded-md bg-custom-grey flex justify-center items-center">
+                                            <ion-icon class="text-white" name="remove"></ion-icon>
+                                        </div>
+                                        <div class="w-[50px] h-[30px] rounded-md bg-grey flex justify-center items-center bg-transparent">
+                                            <span class="font-bold">1</span>
+                                        </div>
+                                        <div class="w-[30px] h-[30px] rounded-md bg-custom-grey flex justify-center items-center">
+                                            <ion-icon class="text-white" name="add"></ion-icon>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <small class="font-custom-tiny">only 12 items left</small>
+                                    </div>
+                                </div>
+                                <div class="space-x-2 flex sm:hidden">
+                                    <div class="items-center">
+                                        <ion-icon class="text-custom-medium" name="trash-bin"></ion-icon>
+                                        <span class="text-custom-tiny text-custom-grey"> Remove</span>
+                                    </div>
+                                    <div class="items-center">
+                                        <ion-icon class="text-custom-medium" name="heart-outline"></ion-icon>
+                                        <span class="text-custom-tiny text-custom-grey"> Add To Wishlists</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end cart -->
+
+            </div>
+
+            <div>
+                <div class="order-container bg-custom-blue text-white rounded-md p-4">
+                    <h2 class="text-custom-extra-large font-bold">Total</h2>
+                    <!-- start itemlists -->
+                    <div class="itemlists-container mb-4">
+                        <div class="item flex justify-between">
+                            <span>item1</span>
+                            <span>23450 Ks</span>
+                        </div>
+                    </div>
+                    <!-- end itemlists -->
+
+                    <div class="line w-full h-[3px] bg-white mb-2"></div>
+
+                    <div class="total flex justify-between font-bold mb-4">
+                        <span>Total</span>
+                        <span>23450 Ks</span>
+                    </div>
+
+                    <button class="checkout-btn bg-custom-orange w-full rounded-md text-white text-custom-extra-large font-bold uppercase py-1">
+                        go to checkout
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END Shopping Cart SECTION -->
+
+    <!-- START footer SECTION -->
+    <section class="bg-custom-blue p-3 static bottom-0">
+        <div class="container mx-auto">
+            <div class="block sm:border-b-2 sm:border-white sm:flex sm:justify-between mb-2">
+                <div class="px-4 py-6 text-white border-b-2 border-white sm:border-none">
+                    <h2 class="text-custom-extra-large font-bold">Quick Link</h2>
+                    <ul class="list-disc px-5">
+                        <li>About Us</li>
+                        <li>Privacy & Policy</li>
+                        <li>How To Buy Guide</li>
+                    </ul>
+                </div>
+
+                <div class="px-4 py-6 text-white border-b-2 border-white sm:border-none">
+                    <h2 class="text-custom-extra-large font-bold">Help ?</h2>
+                    <ul class="list-disc px-5">
+                        <li>Payment</li>
+                        <li>Return & Exchange</li>
+                        <li>FAQ</li>
+                    </ul>
+                </div>
+
+                <div class="px-4 py-6 text-white border-b-2 border-white sm:border-none">
+                    <h2 class="text-custom-extra-large font-bold">Contact Us</h2>
+                    <ul class="px-5">
+                        <li><ion-icon name="call"></ion-icon> +95777777777</li>
+                        <li><ion-icon name="mail"></ion-icon> example@gmial.com</li>
+                        <li class="space-x-3">
+                            <ion-icon name="logo-facebook"></ion-icon>
+                            <ion-icon name="logo-instagram"></ion-icon>
+                            <ion-icon name="logo-twitter"></ion-icon>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <p class="text-center text-white font-bold">©2023 MEGA SHOP All rights reserved.</p>
+        </div>
+    </section>
+    <!-- END footer SECTION -->
+
+</body>
+
+</html>
