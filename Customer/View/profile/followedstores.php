4a9e54eae2b62e373a18cf09ad3920c05a47f63b
Drey <bhon3myatpaing@gmail.com>
2023-07-25 17:33:48 +0630

    profile_mobile_view

--- Customer/View/profile/followedstores.php
+++ Customer/View/profile/followedstores.php
@@ -7,7 +7,7 @@
     <title>Followed Stores</title>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <link rel="stylesheet" href="../resources/lib/tailwind/output.css?id=<?=time() ?>">
+    <link rel="stylesheet" href="../resources/lib/tailwind/output.css?id=<?= time() ?>">
     <script src="https://cdn.tailwindcss.com"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
@@ -56,17 +56,18 @@
         </nav>
     </div>
     <!-- body -->
-    <div class="flex justify-between items-center mt-18 overflow-y-auto mb-10">
+    <div class="flex flex-col md:flex-row justify-evenly items-center mt-20 mb-10">
         <!-- profile section -->
-        <div class="w-1/6 h-1/2 mx-auto bg-gray-100 text-center rounded-xl py-4 mt-36 shadow-2xl">
+        <!-- desktop view -->
+        <div class="hidden md:w-1/6 h-1/2 mx-auto bg-gray-100 text-center rounded-xl py-4 mt-36 shadow-2xl md:flex flex-row md:flex-col">
 
-            <p class="text-xl font-bold py-3">User Profile</p>
+            <p class="text-xl font-bold py-3 hidden md:block">User Profile</p>
 
-            <div class="w-full h-1 bg-black"></div>
+            <div class="w-full h-1 bg-black hidden md:block"></div>
 
-            <p class="text-md py-3 font-light hover:font-semibold cursor-pointer">User Info</p>
+            <p class="text-md py-3 font-semibold hover:font-semibold cursor-pointer">User Info</p>
 
-            <p class="text-md py-3 font-semibold cursor-pointer hover:font-semibold">Followed Stores</p>
+            <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Followed Stores</p>
 
             <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Order History</p>
 
@@ -74,60 +75,121 @@
 
             <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Wishlist</p>
 
-            <div class="flex justify-center items-center">
+            <div class="md:flex justify-center items-center hidden">
                 <ion-icon name="log-out-outline" class="text-xl font-light cursor-pointer hover:font-semibold"></ion-icon>
                 <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Logout</p>
-
+            </div>
+        </div>
+        <!-- mobile view -->
+        <div class="md:hidden mt-4 mb-4">
+            <div class="text-center mb-4">
+                <p class="xl font-bold">My Account</p>
+            </div>
+            <div class="flex justify-evenly items-center">
+                <p class="text-xs ml-2 mr-2 font-semibold border-b-2 border-transparent px-1 py-1 hover:border-black">My Profile</p>
+                <p class="text-xs ml-2 mr-2 font-semibold border-b-2 border-black px-1 py-1 hover:border-black">Followed Stores</p>
+                <p class="text-xs ml-2 mr-2 font-semibold border-b-2 border-transparent px-1 py-1 hover:border-black">Order History</p>
+                <p class="text-xs ml-2 mr-2 font-semibold border-b-2 border-transparent px-1 py-1 hover:border-black">My Reviews</p>
+                <p class="text-xs ml-2 mr-2 font-semibold border-b-2 border-transparent px-1 py-1 hover:border-black">Wishlist</p>
             </div>
         </div>
         <!-- stores -->
-        <div class="w-1/2 h-1/3 mx-auto bg-gray-100 text-center rounded-xl py-4 mt-36 shadow-2xl overscroll-auto">
+        <div class="w-4/5 md:w-1/2 h-1/3 mx-auto bg-gray-100 text-center rounded-xl py-4 mt-20 shadow-2xl overscroll-auto">
             <div class="">
-                <p class="text-2xl text-black font-bold">Followed Stores</p>
+                <p class="text-lg md:text-2xl text-black font-bold">Followed Stores</p>
             </div>
-            <div class="flex flex-wrap justify-evenly px-10 mt-8 mb-8 cursor-pointer">
-                <div class="w-5/12 bg-[#024486] rounded-lg ml-5 mr-5 items-center justify-center shadow-2xl border-2 border-solid">
+            <!-- desktop view -->
+            <div class="hidden md:block">
+            <div class="hidden md:flex flex-wrap justify-evenly px-2 md:px-10 mt-8 mb-8 cursor-pointer">
+                <div class="w-1/4 md:w-5/12 bg-[#024486] rounded-lg ml-1 mr-1 md:ml-5 md:mr-5 items-center justify-center shadow-2xl border-2 border-solid">
                     <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
                     <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-5">
                         <p class="text-white text-2xl font-semibold">Adidas</p>
                     </div>
                 </div>
-                <div class="w-5/12 bg-[#024486] rounded-lg ml-5 mr-5 items-center justify-center shadow-2xl border-2 border-solid">
+                <div class="w-1/4 md:w-5/12 bg-[#024486] rounded-lg ml-1 mr-1 md:ml-5 md:mr-5 items-center justify-center shadow-2xl border-2 border-solid">
                     <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
                     <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-5">
-                        <p class="text-white text-2xl font-semibold">Adidas</p>
+                        <p class="text-white text-sm md:text-2xl font-semibold">Adidas</p>
                     </div>
                 </div>
 
             </div>
-            <div class="flex flex-wrap justify-evenly px-10 mt-8 mb-8 cursor-pointer">
-                <div class="w-5/12 bg-[#024486] rounded-lg ml-5 mr-5 items-center justify-center shadow-2xl border-2 border-solid">
+            <div class="hidden md:flex flex-wrap justify-evenly px-2 md:px-10 mt-8 mb-8 cursor-pointer">
+                <div class="w-1/4 md:w-5/12 bg-[#024486] rounded-lg ml-1 mr-1 md:ml-5 md:mr-5 items-center justify-center shadow-2xl border-2 border-solid">
                     <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
                     <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-5">
                         <p class="text-white text-2xl font-semibold">Adidas</p>
                     </div>
                 </div>
-                <div class="w-5/12 bg-[#024486] rounded-lg ml-5 mr-5 items-center justify-center shadow-2xl border-2 border-solid">
+                <div class="w-1/4 md:w-5/12 bg-[#024486] rounded-lg ml-1 mr-1 md:ml-5 md:mr-5 items-center justify-center shadow-2xl border-2 border-solid">
                     <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
                     <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-5">
-                        <p class="text-white text-2xl font-semibold">Adidas</p>
+                        <p class="text-white text-sm md:text-2xl font-semibold">Adidas</p>
                     </div>
                 </div>
 
             </div>
-            <div class="flex flex-wrap justify-evenly px-10 mt-8 mb-8 cursor-pointer">
-                <div class="w-5/12 bg-[#024486] rounded-lg ml-5 mr-5 items-center justify-center shadow-2xl border-2 border-solid">
+            <div class="hidden md:flex flex-wrap justify-evenly px-2 md:px-10 mt-8 mb-8 cursor-pointer">
+                <div class="w-1/4 md:w-5/12 bg-[#024486] rounded-lg ml-1 mr-1 md:ml-5 md:mr-5 items-center justify-center shadow-2xl border-2 border-solid">
                     <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
                     <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-5">
                         <p class="text-white text-2xl font-semibold">Adidas</p>
                     </div>
                 </div>
-                <div class="w-5/12 bg-[#024486] rounded-lg ml-5 mr-5 items-center justify-center shadow-2xl border-2 border-solid">
+                <div class="w-1/4 md:w-5/12 bg-[#024486] rounded-lg ml-1 mr-1 md:ml-5 md:mr-5 items-center justify-center shadow-2xl border-2 border-solid">
                     <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
                     <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-5">
-                        <p class="text-white text-2xl font-semibold">Adidas</p>
+                        <p class="text-white text-sm md:text-2xl font-semibold">Adidas</p>
                     </div>
                 </div>
+
+            </div>
+            </div>
+            <!-- mobile view -->
+            <div class="md:hidden"> 
+            <div class="flex flex-wrap justify-evenly mt-5">
+                <div class="w-40 md:w-5/12 bg-[#024486] rounded-lg  items-center justify-center shadow-2xl border-2 border-solid">
+                    <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
+                    <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-2">
+                        <p class="text-white text-lg font-semibold">Adidas</p>
+                    </div>
+                </div>
+                <div class="w-40 md:w-5/12 bg-[#024486] rounded-lg  items-center justify-center shadow-2xl border-2 border-solid">
+                    <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
+                    <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-2">
+                        <p class="text-white text-lg font-semibold">Adidas</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-wrap justify-evenly mt-5">
+                <div class="w-40 md:w-5/12 bg-[#024486] rounded-lg  items-center justify-center shadow-2xl border-2 border-solid">
+                    <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
+                    <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-2">
+                        <p class="text-white text-lg font-semibold">Adidas</p>
+                    </div>
+                </div>
+                <div class="w-40 md:w-5/12 bg-[#024486] rounded-lg  items-center justify-center shadow-2xl border-2 border-solid">
+                    <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
+                    <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-2">
+                        <p class="text-white text-lg font-semibold">Adidas</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-wrap justify-evenly mt-5">
+                <div class="w-40 md:w-5/12 bg-[#024486] rounded-lg  items-center justify-center shadow-2xl border-2 border-solid">
+                    <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
+                    <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-2">
+                        <p class="text-white text-lg font-semibold">Adidas</p>
+                    </div>
+                </div>
+                <div class="w-40 md:w-5/12 bg-[#024486] rounded-lg  items-center justify-center shadow-2xl border-2 border-solid">
+                    <img src="../resources/img/adidas-logo-svgrepo-com.svg" alt="" class="w-4/5 mx-auto mt-5 rounded-lg">
+                    <div class="bg-orange-400 w-full mb-0 mt-8 rounded-br-lg rounded-bl-lg py-2">
+                        <p class="text-white text-lg font-semibold">Adidas</p>
+                    </div>
+                </div>
+            </div>
 
             </div>
         </div>
--- Customer/View/profile/profile.php
+++ Customer/View/profile/profile.php
@@ -59,7 +59,7 @@
     <!-- body -->
     <div class="flex flex-col md:flex-row justify-evenly items-center mt-20 mb-10">
         <!-- desktop view -->
-        <div class="md:w-1/6 h-1/2 mx-auto bg-gray-100 text-center rounded-xl py-4 mt-36 shadow-2xl flex flex-row md:flex-col">
+        <div class="hidden md:w-1/6 h-1/2 mx-auto bg-gray-100 text-center rounded-xl py-4 mt-36 shadow-2xl md:flex flex-row md:flex-col">
 
             <p class="text-xl font-bold py-3 hidden md:block">User Profile</p>
 
@@ -81,17 +81,26 @@
             </div>
         </div>
         <!-- mobile view -->
-        <div>
-
+        <div class="md:hidden mt-4 mb-4">
+            <div class="text-center mb-4">
+                <p class="xl font-bold">My Account</p>
+            </div>
+            <div class="flex justify-evenly items-center">
+            <p class="text-xs ml-2 mr-2 font-semibold border-b-2 border-black px-1 py-1 hover:shadow-xl">My Profile</p>
+            <p class="text-xs ml-2 mr-2 font-semibold px-1 py-1">Followed Stores</p>
+            <p class="text-xs ml-2 mr-2 font-semibold px-1 py-1">Order History</p>
+            <p class="text-xs ml-2 mr-2 font-semibold px-1 py-1">My Reviews</p>
+            <p class="text-xs ml-2 mr-2 font-semibold px-1 py-1">Wishlist</p>
+            </div>
         </div>
         <!-- edit -->
-        <div class="w-2/3 md:w-5/12 h-1/2 mx-auto bg-gray-100 shadow-2xl text-center rounded-xl py-4 mt-5">
+        <div class="w-4/5 md:w-5/12 h-1/2 mx-auto bg-gray-100 shadow-2xl text-center rounded-xl py-4 mt-5 border-2 border-transparent">
             <div class="flex  items-center justify-evenly">
                 <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-1/3 md:w-1/6 h-1/6">
                     <path d="M22.8824 1.56689C11.0449 1.56689 1.44922 11.1625 1.44922 23.0001C1.44922 34.8377 11.0449 44.4333 22.8824 44.4333C34.72 44.4333 44.3156 34.8377 44.3156 23.0001C44.3156 11.1625 34.72 1.56689 22.8824 1.56689Z" stroke="#024486" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                     <path d="M6.31738 36.6016C6.31738 36.6016 11.0948 30.5017 22.8831 30.5017C34.6714 30.5017 39.451 36.6016 39.451 36.6016M22.8831 23.0001C24.5884 23.0001 26.2239 22.3226 27.4298 21.1168C28.6356 19.9109 29.3131 18.2754 29.3131 16.5701C29.3131 14.8648 28.6356 13.2293 27.4298 12.0234C26.2239 10.8176 24.5884 10.1401 22.8831 10.1401C21.1778 10.1401 19.5423 10.8176 18.3364 12.0234C17.1306 13.2293 16.4531 14.8648 16.4531 16.5701C16.4531 18.2754 17.1306 19.9109 18.3364 21.1168C19.5423 22.3226 21.1778 23.0001 22.8831 23.0001Z" stroke="#024486" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
-                <div class="flex flex-col items-center">
+                <div class="hidden md:flex flex-col items-center">
                     <p class="text-xl font-bold py-6">David</p>
                     <p class="text-lg font-semibold py-6">Yangon, Myanmar</p>
                 </div>
