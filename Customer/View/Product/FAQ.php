702cc00d5aa60faca6237043864fcf2cc21a8131
Drey <bhon3myatpaing@gmail.com>
2023-07-25 15:02:55 +0630

    admin nav not complete

--- Customer/View/Product/FAQ.php
+++ Customer/View/Product/FAQ.php
@@ -23,7 +23,7 @@
         }
     </style>
 </head> 
-<body class="bg-slate-100 font-['Poppins'] no-scrollbar flex flex-col min-h-screen">
+<body class="bg-slate-100 font-['Poppins'] no-scrollbar md:flex flex-col min-h-screen">
     <!-- navbar responsive -->
     <div class="w-full flex flex-col items-center">
         <!-- navbar -->
