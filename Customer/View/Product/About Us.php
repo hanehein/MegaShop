d83a83001a21946e829612e7911f0035b21655a9
Drey <bhon3myatpaing@gmail.com>
2023-07-26 01:39:16 +0630

    admin_nav update

--- Customer/View/Product/About Us.php
+++ Customer/View/Product/About Us.php
@@ -19,6 +19,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
     <style>
         .no-scrollbar::-webkit-scrollbar {
             display: none;
@@ -30,29 +31,79 @@
     <!-- navbar responsive -->
     <div class="w-full flex flex-col items-center">
         <!-- navbar -->
-        <nav class="w-full h-14 bg-white flex justify-between items-center px-4 z-50 fixed">
-            <div class=" md:hidden">
-                <ion-icon name="menu" size="large" class="text-[#024486] hover:text-[#F68721]"></ion-icon>
+        <nav class="bg-white border-gray-200 w-full">
+            <div class="w-full flex flex-wrap items-center justify-between mx-auto py-2 px-1">
+                <!-- logo -->
+                <a href="#" class="flex flex-col items-center justify-center font-['Wallpoet'] text-2xl text-[#024486] leading-none cursor-pointer">
+                    <p class="">MEGA</p>
+                    <p>SHOP</p>
+                </a>
+                <!-- humbermenu -->
+                <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-[#024486] rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-dropdown" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
+                <!-- ul lists -->
+                <div class="hidden w-full md:flex md:items-center md:justify-center md:w-auto" id="navbar-dropdown">
+                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:flex-wrap md:space-x-8 md:mt-0 md:border-0 md:bg-white  font-['Poppins']">
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Home</a>
+                        </li>
+                        <li>
+                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto">Products<svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                                </svg></button>
+                            <!-- Dropdown menu -->
+                            <div id="dropdownNavbar" class="z-10 hidden font-normal bg-slate-200 divide-y divide-gray-100 rounded-lg shadow w-44 ">
+                                <div class="py-1">
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-800 hover:text-white hover:bg-[#024486]">Categories</a>
+                                </div>
+                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Fashion</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Home & Lifestyles</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Health & Beauty</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Kitchen ware</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Electronics</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Babies & Toys</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Sports & Outdoors</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Shops</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contact Us</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Sing In</a>
+                        </li>
+                    </ul>
+                    <div class="md:flex md:items-center md:justify-center cursor-pointer md:ml-6">
+                        <a href="#" class="block md:py-2 md:pl-3 md:pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"><ion-icon name="cart" size="large" class="text-[#003366] hover:text-[#F68721] "></ion-icon><ion-icon name="ellipse" class="text-[#F68721] mb-6 hover:text-[#003366]"></ion-icon></a>
+                        <a href="#" class="block md:py-2 md:pl-3 md:pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"><ion-icon name="person-circle" size="large" class="text-[#003366] md:pl-4 hover:text-[#F68721]"></ion-icon></a>
+                    </div>
+                </div>
             </div>
-            <div class="font-['Wallpoet'] text-2xl text-[#024486] leading-none cursor-pointer">
-                <p class="">MEGA</p>
-                <p>SHOP</p>
-            </div>
-            <div class="md:w-auto hidden md:flex px-2 py-2">
-                <ul class="uppercase text-[#003366] text-lg font-semibold flex items-center space-x-5 font-['Poppins'] cursor-pointer">
-                    <li class="border-2 border-transparent px-2 py-2 hover:text-[#F68721] hover:border-b-blue-800 text-sm"><a href="">Home</a></li>
-                    <li class="border-2 border-transparent px-2 py-2 hover:text-[#F68721] hover:border-b-blue-800 text-sm"><a href="">Products</a></li>
-                    <li class="border-2 border-transparent px-2 py-2 hover:text-[#F68721] hover:border-b-blue-800 text-sm"><a href="">Shops</a></li>
-                    <li class="border-2 border-transparent px-2 py-2 hover:text-[#F68721] hover:border-b-blue-800 text-sm"><a href="">About US</a></li>
-                    <li class="border-2 border-transparent px-2 py-2 hover:text-[#F68721] hover:border-b-blue-800 text-sm"><a href="">Contact US</a></li>
-                </ul>
-            </div>
-            <div class="flex items-center justify-center space-x-2 cursor-pointer">
-                <ion-icon name="cart" size="large" class="text-[#003366] hover:text-[#F68721] "></ion-icon>
-                <ion-icon name="ellipse" class="text-[#F68721] mb-6 hover:text-[#003366]"></ion-icon>
-                <ion-icon name="person-circle" size="large" class="text-[#003366] pl-4 hover:text-[#F68721]"></ion-icon>
-            </div>
-
         </nav>
     </div>
     
@@ -145,6 +196,7 @@
             <p class="text-white text-sm font-semibold">&#64;2023 MEGA SHOP All Rights Reserved.</p>
         </div>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
 </body>
 
 </html>
--- Customer/View/profile/wishlist.php
+++ Customer/View/profile/wishlist.php
@@ -19,6 +19,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
     <style>
         .no-scrollbar::-webkit-scrollbar {
             display: none;
@@ -29,40 +30,91 @@
 <body class="bg-[#F6F6F6] font-['Poppins] no-scrollbar flex flex-col min-h-screen">
     <div class="w-full flex flex-col items-center">
         <!-- navbar -->
-        <nav class="w-full h-14 bg-white flex justify-between items-center px-4 z-50 fixed">
-            <div class=" md:hidden">
-                <ion-icon name="menu" size="large" class="text-[#024486] hover:text-[#F68721]"></ion-icon>
-            </div>
-            <div class="font-['Wallpoet'] text-2xl text-[#024486] leading-none cursor-pointer">
-                <p class="">MEGA</p>
-                <p>SHOP</p>
-            </div>
-            <div class="md:w-auto hidden md:flex px-2 py-2">
-                <ul class="uppercase text-[#003366] text-lg font-semibold flex items-center space-x-5 font-['Poppins'] cursor-pointer">
-                    <li class="border-2 border-transparent px-2 py-2 hover:text-[#F68721] hover:border-b-blue-800 text-sm"><a href="">Home</a></li>
-                    <li class="border-2 border-transparent px-2 py-2 hover:text-[#F68721] hover:border-b-blue-800 text-sm"><a href="">Products</a></li>
-                    <li class="border-2 border-transparent px-2 py-2 hover:text-[#F68721] hover:border-b-blue-800 text-sm"><a href="">Shops</a></li>
-                    <li class="border-2 border-transparent px-2 py-2 hover:text-[#F68721] hover:border-b-blue-800 text-sm"><a href="">About US</a></li>
-                    <li class="border-2 border-transparent px-2 py-2 hover:text-[#F68721] hover:border-b-blue-800 text-sm"><a href="">Contact US</a></li>
-                </ul>
-            </div>
-            <div class="flex items-center justify-center space-x-2 cursor-pointer">
-                <ion-icon name="cart" size="large" class="text-[#003366] hover:text-[#F68721] "></ion-icon>
-                <ion-icon name="ellipse" class="text-[#F68721] mb-6 hover:text-[#003366]"></ion-icon>
-                <ion-icon name="person-circle" size="large" class="text-[#003366] pl-4 hover:text-[#F68721]"></ion-icon>
+        <nav class="bg-white border-gray-200 w-full">
+            <div class="w-full flex flex-wrap items-center justify-between mx-auto py-2 px-1">
+                <!-- logo -->
+                <a href="#" class="flex flex-col items-center justify-center font-['Wallpoet'] text-2xl text-[#024486] leading-none cursor-pointer">
+                    <p class="">MEGA</p>
+                    <p>SHOP</p>
+                </a>
+                <!-- humbermenu -->
+                <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-[#024486] rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-dropdown" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
+                <!-- ul lists -->
+                <div class="hidden w-full md:flex md:items-center md:justify-center md:w-auto" id="navbar-dropdown">
+                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:flex-wrap md:space-x-8 md:mt-0 md:border-0 md:bg-white  font-['Poppins']">
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Home</a>
+                        </li>
+                        <li>
+                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto">Products<svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                                </svg></button>
+                            <!-- Dropdown menu -->
+                            <div id="dropdownNavbar" class="z-10 hidden font-normal bg-slate-200 divide-y divide-gray-100 rounded-lg shadow w-44 ">
+                                <div class="py-1">
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-800 hover:text-white hover:bg-[#024486]">Categories</a>
+                                </div>
+                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Fashion</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Home & Lifestyles</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Health & Beauty</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Kitchen ware</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Electronics</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Babies & Toys</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-4 py-2 text-gray-800 hover:text-white hover:bg-[#024486]">Sports & Outdoors</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Shops</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contact Us</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Sing In</a>
+                        </li>
+                    </ul>
+                    <div class="md:flex md:items-center md:justify-center cursor-pointer md:ml-6">
+                        <a href="#" class="block md:py-2 md:pl-3 md:pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"><ion-icon name="cart" size="large" class="text-[#003366] hover:text-[#F68721] "></ion-icon><ion-icon name="ellipse" class="text-[#F68721] mb-6 hover:text-[#003366]"></ion-icon></a>
+                        <a href="#" class="block md:py-2 md:pl-3 md:pr-4 text-[#024486] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"><ion-icon name="person-circle" size="large" class="text-[#003366] md:pl-4 hover:text-[#F68721]"></ion-icon></a>
+                    </div>
+                </div>
             </div>
         </nav>
     </div>
     <!-- body -->
-    <div class="flex justify-between items-center mt-18 overflow-y-auto mb-10">
+    <div class="flex flex-col md:flex-row md:justify-between items-center mt-18 overflow-y-auto mb-10">
         <!-- profile section -->
-        <div class="md:w-1/6 h-1/2 mx-auto bg-gray-100 text-center rounded-xl py-4 mt-36 shadow-2xl">
+        <div class="hidden md:w-1/6 h-1/2 mx-auto bg-gray-100 text-center rounded-xl py-4 mt-36 shadow-2xl md:flex flex-row md:flex-col">
 
-            <p class="text-xl font-bold py-3">User Profile</p>
+            <p class="text-xl font-bold py-3 hidden md:block">User Profile</p>
 
-            <div class="w-full h-1 bg-black"></div>
+            <div class="w-full h-1 bg-black hidden md:block"></div>
 
-            <p class="text-md py-3 font-light hover:font-semibold cursor-pointer">User Info</p>
+            <p class="text-md py-3 font-semibold hover:font-semibold cursor-pointer">User Info</p>
 
             <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Followed Stores</p>
 
@@ -70,17 +122,30 @@
 
             <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">My Reviews</p>
 
-            <p class="text-md py-3 font-semibold cursor-pointer hover:font-semibold">Wishlist</p>
+            <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Wishlist</p>
 
-            <div class="flex justify-center items-center">
+            <div class="md:flex justify-center items-center hidden">
                 <ion-icon name="log-out-outline" class="text-xl font-light cursor-pointer hover:font-semibold"></ion-icon>
                 <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Logout</p>
-
+            </div>
+        </div>
+        <!-- mobile view -->
+        <div class="md:hidden mt-4 mb-4">
+            <div class="text-center mb-4">
+                <p class="xl font-bold">My Account</p>
+            </div>
+            <div class="flex justify-evenly items-center">
+                <p class="text-xs ml-2 mr-2 font-semibold px-1 py-1">My Profile</p>
+                <p class="text-xs ml-2 mr-2 font-semibold px-1 py-1">Followed Stores</p>
+                <p class="text-xs ml-2 mr-2 font-semibold px-1 py-1">Order History</p>
+                <p class="text-xs ml-2 mr-2 font-semibold px-1 py-1">My Reviews</p>
+                <p class="text-xs ml-2 mr-2 font-semibold border-b-2 border-black px-1 py-1 hover:shadow-xl">Wishlist</p>
             </div>
         </div>
         <!-- wishlist -->
-        <div class="w-1/2 items-center flex mx-auto mt-20">
-            <div class="flex flex-col items-center justify-center ">
+        <div class="w-1/2 md:w-1/2 items-center flex mx-auto mt-20 justify-center">
+            <!-- desktop view -->
+            <div class="hidden md:flex flex-col items-center justify-center ">
                 <p class="text-xl font-semibold">
                     Wishlist
                 </p>
@@ -177,6 +242,50 @@
                         </div>
 
                     </div>
+                    
+                </div>
+                
+
+            </div>
+            <!-- mobile view -->
+            <div class="md:hidden flex flex-col justify-center">
+                <p class="text-xl font-semibold text-center">
+                    Wishlist
+                </p>
+                <div class="w-full">
+                    <div class="bg-[#d6d3d1] mt-10 rounded-lg mb-10">
+                        <div class="flex justify-around bg-[#003366] w-full rounded-tr-lg rounded-tl-lg px-2 py-2">
+
+                            <p class="text-white text-sm md:text-lg font-bold mr-10 md:mr-72">Products</p>
+                            <p class="text-white text-sm md:text-lg font-bold md:ml-20">Unit Price</p>
+                            <p class="text-white text-sm md:text-lg font-bold">Stock Status</p>
+                            <p class="text-white text-sm md:text-lg font-bold">Action</p>
+
+                        </div>
+
+                    </div>
+
+                    <div class="rounded-lg   bg-[#FFFFFF] shadow-2xl mb-5 mt-5 ml-3 mr-3 border-solid border-2">
+
+                        <div class="flex items-center justify-between">
+                            <img src="../resources/img/backpack 1.jpg" alt="" class="w-1/4 md:w-1/6 h-1/3 ml-10 mt-3 rounded-lg mb-3">
+                            <div class="py-3 text-left mr-10 md:mr-20">
+                                <p class="font-semibold md:text-lg mb-2 mt-2"> Tucano Backpacks TU-BP01</p>
+                                <p class="md:mb-2 text-xs md:text-md md:mt-2">Tucano, Color Family:Blue</p>
+                                <p class="md:mb-2 text-xs md:text-md md:mt-2">Color :Blue</p>
+                                <p class="md:mb-2 text-xs md:text-md md:mt-2">Size : ###</p>
+                            </div>
+
+                            <p class="md:text-xl font-semibold md:mr-10">100$</p>
+
+                            <p class="md:text-xl font-semibold">In Stock</p>
+
+
+                            <button class="w-auto bg-[#dc2626] px-2 py-2 text-white rounded-lg shadow-xl">Remove</button>
+
+                        </div>
+
+                    </div>
                 </div>
 
             </div>
@@ -219,6 +328,7 @@
             <p class="text-white text-sm font-semibold">&#64;2023 MEGA SHOP All Rights Reserved.</p>
         </div>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
 </body>
 
 </html>
