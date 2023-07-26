b8b5a5bbad1f08fea691aa273bf871e95bb5d125
Drey <bhon3myatpaing@gmail.com>
2023-07-26 11:44:41 +0630

    uiedit

--- Admin/View/adminDashboard/uiedit.php
+++ Admin/View/adminDashboard/uiedit.php
@@ -18,17 +18,16 @@
 <body class="flex justify-center items-center overflow-x-hidden">
     <!-- sidebar -->
     <div class="w-1/3 min-h-screen flex flex-col flex-auto bg-gray-50 text-gray-800">
-        <div class="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
-            <div class="flex justify-center">
-                <div class="mr-20">
-                    <ion-icon name="menu" size="large" class="text-[#024486] hover:text-[#F68721]"></ion-icon>
-                </div>
+    <div class="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
+            <div class="flex justify-center items-center mt-3">
                 <div class="font-['Wallpoet'] text-2xl text-[#024486] leading-none cursor-pointer">
                     <p class="">MEGA</p>
                     <p>SHOP</p>
                 </div>
             </div>
-            <div class="overflow-y-auto overflow-x-hidden flex-grow">
+            <!-- Menu -->
+            <div class="overflow-y-auto overflow-x-hidden flex-grow font-['Poppins']">
+                <!-- ul list -->
                 <ul class="flex flex-col py-4 space-y-1">
                     <li class="px-5">
                         <div class="flex flex-row items-center h-8">
@@ -36,69 +35,69 @@
                         </div>
                     </li>
                     <li>
-                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
                             <span class="inline-flex justify-center items-center ml-4">
-                                <ion-icon name="grid"></ion-icon>
+                                <ion-icon name="grid" class=""></ion-icon>
                             </span>
-                            <span class="ml-2 text-sm tracking-wide truncate">Dashboard</span>
+                            <span class="ml-2 text-sm font-semibold tracking-wide truncate">Dashboard</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
                             <span class="inline-flex justify-center items-center ml-4">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path>
-                                </svg>
+                                <ion-icon name="diamond"></ion-icon>
                             </span>
-                            <span class="ml-2 text-sm tracking-wide truncate">Inbox</span>
-                            <span class="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-indigo-500 bg-indigo-50 rounded-full">New</span>
+                            <span class="ml-2 text-sm font-semibold tracking-wide truncate">Income</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
                             <span class="inline-flex justify-center items-center ml-4">
                                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path>
                                 </svg>
                             </span>
-                            <span class="ml-2 text-sm tracking-wide truncate">Messages</span>
+                            <span class="ml-2 text-sm tracking-wide truncate font-semibold">Messages</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
                             <span class="inline-flex justify-center items-center ml-4">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
-                                </svg>
+                                <ion-icon name="people"></ion-icon>
                             </span>
-                            <span class="ml-2 text-sm tracking-wide truncate">Notifications</span>
-                            <span class="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-500 bg-red-50 rounded-full">1.2k</span>
-                        </a>
-                    </li>
-                    <li class="px-5">
-                        <div class="flex flex-row items-center h-8">
-                            <div class="text-sm font-light tracking-wide text-gray-500">Tasks</div>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
-                            <span class="inline-flex justify-center items-center ml-4">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
-                                </svg>
-                            </span>
-                            <span class="ml-2 text-sm tracking-wide truncate">Available Tasks</span>
+                            <span class="ml-2 text-sm font-semibold tracking-wide truncate">Supplier Lists</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
                             <span class="inline-flex justify-center items-center ml-4">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
-                                </svg>
+                                <ion-icon name="people"></ion-icon>
                             </span>
-                            <span class="ml-2 text-sm tracking-wide truncate">Clients</span>
-                            <span class="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-green-500 bg-green-50 rounded-full">15</span>
+                            <span class="ml-2 text-sm font-semibold tracking-wide truncate">Customer Lists</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
+                            <span class="inline-flex justify-center items-center ml-4">
+                            <ion-icon name="list-circle"></ion-icon>
+                            </span>
+                            <span class="ml-2 text-sm font-semibold tracking-wide truncate">Proudcts Lists</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
+                            <span class="inline-flex justify-center items-center ml-4">
+                            <ion-icon name="cube"></ion-icon>
+                            </span>
+                            <span class="ml-2 text-sm font-semibold tracking-wide truncate">Order Lists</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
+                            <span class="inline-flex justify-center items-center ml-4">
+                            <ion-icon name="help-circle"></ion-icon>
+                            </span>
+                            <span class="ml-2 text-sm font-semibold tracking-wide truncate">FAQ</span>
                         </a>
                     </li>
                     <li class="px-5">
@@ -107,46 +106,72 @@
                         </div>
                     </li>
                     <li>
-                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
                             <span class="inline-flex justify-center items-center ml-4">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-                                </svg>
+                            <ion-icon name="person-circle"></ion-icon>
                             </span>
-                            <span class="ml-2 text-sm tracking-wide truncate">Profile</span>
+                            <span class="ml-2 text-sm font-semibold tracking-wide truncate">Admin Profile</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
                             <span class="inline-flex justify-center items-center ml-4">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                </svg>
+                            <ion-icon name="apps"></ion-icon>
                             </span>
-                            <span class="ml-2 text-sm tracking-wide truncate">Settings</span>
+                            <span class="ml-2 text-sm font-semibold tracking-wide truncate">Web UI</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                    <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-100 text-[#003366] border-l-4 border-transparent hover:border-blue-500 pr-6 active:bg-[#003366] active:border-none active:text-white">
                             <span class="inline-flex justify-center items-center ml-4">
-                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
-                                </svg>
+                            <ion-icon name="log-out"></ion-icon>
                             </span>
-                            <span class="ml-2 text-sm tracking-wide truncate">Logout</span>
+                            <span class="ml-2 text-sm font-semibold tracking-wide truncate">Logout</span>
                         </a>
                     </li>
                 </ul>
             </div>
         </div>
         <!-- nav -->
-        <div class="w-full h-auto flex justify-center items-center bg-[#003366] px-5 py-5 ">
+        <div class="w-full h-auto flex justify-center items-center bg-[#003366] px-5 py-5 ml-64">
             <input type="text" class="w-1/3 mx-auto py-2 rounded-lg" placeholder="Search">
-            <p class="text-xl text-white">
+            <p class="text-lg font-semibold text-white mx-auto">
                 28 July 2023
             </p>
+
         </div>
+
+        <div class="w-1/2 h-auto mx-auto flex justify-center items-center bg-[#FFFFFF] px-7 py-7 ">
+            <div class="w-full h-auto flex flex-col justify-center items-start px-5 py-5 bg-[#d1d5db] rounded-md">
+                <div class="mt-2 mb-3">
+                    <p class="text-[#003366] text-2xl font-semibold">
+                        Website Form
+                    </p>
+                </div>
+                <div class="text-[#003366] mt-2 mb-3">
+                    <p>Website Logo</p>
+                </div>
+                <div class="w-1/6 h-auto bg-white flex flex-col items-center justify-center py-3 px-3 rounded-lg mt-2 mb-3">
+                    <div class="font-['Wallpoet'] text-2xl text-[#024486] leading-none cursor-pointer mx-auto">
+                        <p class="">MEGA</p>
+                        <p>SHOP</p>
+                    </div>
+                </div>
+
+                <button class="px-3 py-2 bg-[#FFFFFF] rounded-lg shadow-xl border-2 border-black mt-2 mb-3">Change Logo</button>
+                <div class="text-[#003366] mt-2 mb-3">
+                    <p>Website Name</p>
+                </div>
+                <input type="text" class="mt-2 mb-3 rounded-lg px-2 w-1/3">
+                <div class="text-[#003366] mt-2 mb-3">
+                    <p>Slogan</p>
+                </div>
+                <input type="text" class="mt-2 mb-3 rounded-lg px-2 w-1/3">
+                <button class=" px-5 py-2 bg-custom-blue text-white font-semibold rounded-lg float-right mt-2 mb-3 hover:bg-[#9ca3af]">Save</button>
+            </div>
+
+        </div>
+
 
     </div>
     <!-- nav -->
