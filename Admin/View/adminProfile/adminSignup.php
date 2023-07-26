a076a3222881e2c8ee63a288eb24918fc173f320
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2023-07-26 18:56:53 +0630

    complete sing up/login

--- Admin/View/adminProfile/adminSignup.php
+++ Admin/View/adminProfile/adminSignup.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>Admin Signup</title>
     <link href="../resources/lib/tailwind/output.css?id=<?= time() ?>" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
@@ -15,88 +15,52 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
 </head>
 
-<body class="overflow-x-hidden bg-black">
+<body class="overflow-x-hidden bg-[#F6F6F6]">
     <!-- main div -->
-    <div class="w-auto h-screen container flex mx-auto justify-center items-center">
-      <!-- inner container -->
-      <div
-        class="md:w-[30rem] w-[18rem] container mx-auto flex-col items-center justify-center bg-[#2A2A2A] rounded-md"
-      >
-        <!-- logo section -->
-        <div class="flex space-x-1.5 items-center justify-center">
-          <div>
-            <ion-icon
-              name="game-controller"
-              size="large"
-              class="text-white pt-5"
-            ></ion-icon>
-          </div>
-          <div class="text-white text-9xl">
-            <p class="text-lg font-['Iceberg']">MAX</p>
-            <p class="text-sm font-['Iceberg'] leading-[8px]">Gaming Store</p>
-          </div>
+    <!-- Outer container -->
+    <div class="w-auto h-screen container flex mx-auto justify-center items-center ">
+        <!-- inner container -->
+        <div class="md:w-[30rem] w-[18rem] container mx-auto flex-col items-center justify-center bg-[#003366] rounded-bl-3xl rounded-tr-3xl pt-8 pb-4 shadow-md shadow-black">
+            <!-- logo section -->
+            <div class="flex flex-col space-x-1.5 items-center justify-center">
+                <div class="font-['Wallpoet'] text-2xl text-white leading-none cursor-pointer ">
+                    <p class="">MEGA</p>
+                    <p>SHOP</p>
+                </div>
+            </div>
+            <!-- Inner Container section -->
+            <div class="container md:w-[25rem] mx-auto font-['Poppins']  flex-col justify-center items-center py-5">
+                <!-- Sign up section -->
+                <p class="text-white text-lg text-center  ">Create New Account</p>
+                
+                <form action="">
+                    <!-- username -->
+                    <div class="shadow-md shadow-black md:w-[18rem] w-[16rem] bg-[#F6F6F6] border border-white rounded-[5px] mx-auto mt-4 flex py-1.5 px-4 space-x-5 justify-center items-center">
+                        <ion-icon name="person" class="text-[#003366] "></ion-icon>
+                        <input type="text" name="" id="name" placeholder="Username" class="text-sm bg-[#F6F6F6] focus:outline-none text-white border-none ">
+                    </div>
+                    <!-- email -->
+                    <div class="shadow-md shadow-black md:w-[18rem] w-[16rem] bg-[#F6F6F6] border border-white rounded-[5px] mx-auto mt-7 flex py-1.5 px-4 space-x-5 justify-center items-center">
+                        <ion-icon name="mail" class="text-[#003366]"></ion-icon>
+                        <input type="email" name="" id="email" placeholder="Email Address" class="text-sm bg-[#F6F6F6] focus:outline-none text-white border-none" />
+                    </div>
+                    <!-- password -->
+                    <div class="shadow-md shadow-black md:w-[18rem] w-[16rem] bg-[#F6F6F6] border border-white rounded-[5px] mx-auto mt-7 flex py-1.5 px-4 space-x-5 justify-center items-center">
+                        <ion-icon name="lock-closed" class="text-[#003366]"></ion-icon>
+                        <input type="password" name="" id="password" placeholder="Password" class="text-sm bg-[#F6F6F6] focus:outline-none text-white border-none" />
+                    </div>
+                    <!-- phone number -->
+                    <div class="shadow-md shadow-black md:w-[18rem] w-[16rem] bg-[#F6F6F6] border border-white rounded-[5px] mx-auto mt-7 flex py-1.5 px-4 space-x-5 justify-center items-center">
+                    <ion-icon name="phone-portrait" class="text-[#003366]" ></ion-icon>
+                        <input type="password" name="" id="password" placeholder="Enter Phone Number" class="text-sm bg-[#F6F6F6] focus:outline-none text-white border-none" />
+                    </div>
+                    <!-- login -->
+                    <button type="submit" class="w-[10rem]  bg-blue-500 rounded-[8px] mx-auto mt-8 flex py-2 justify-center items-center shadow-md shadow-black hover:bg-white hover:text-gray-500 text-white font-semibold ">
+                        Sign Up
+                    </button>
+                </form>
+            </div>
         </div>
-        <!-- Inner Container section -->
-        <div class="container md:w-[25rem] mx-auto font-['Inter']  flex-col justify-center items-center py-5">
-          <!-- Sign up section -->
-          <p class="text-white text-[22px] font-bold text-center ">Sign Up </p>
-          <!-- username -->
-          <div
-            class="md:w-[18rem] w-[16rem] bg-[#2A2A2A] border border-white rounded-[5px] mx-auto mt-4 flex py-1.5 px-4 space-x-5 justify-center items-center"
-          >
-            <ion-icon name="person" class="text-[#A19393]"></ion-icon>
-            <input type="text" name="" id="name" placeholder="Username"
-            class="text-sm bg-[#2A2A2A] focus:outline-none text-white">
-          </div>
-          <!-- email -->
-          <div
-            class="md:w-[18rem] w-[16rem] bg-[#2A2A2A] border border-white rounded-[5px] mx-auto mt-7 flex py-1.5 px-4 space-x-5 justify-center items-center"
-          >
-            <ion-icon name="mail" class="text-[#A19393]"></ion-icon>
-            <input
-              type="email"
-              name=""
-              id="email"
-              placeholder="Email Address"
-              class="text-sm bg-[#2A2A2A] focus:outline-none text-white"
-            />
-          </div>
-          <!-- password -->
-          <div
-            class="md:w-[18rem] w-[16rem] bg-[#2A2A2A] border border-white rounded-[5px] mx-auto mt-7 flex py-1.5 px-4 space-x-5 justify-center items-center"
-          >
-          <ion-icon name="lock-closed" class="text-[#A19393]"></ion-icon>
-          <input
-              type="password"
-              name=""
-              id="password"
-              placeholder="Password"
-              class="text-sm bg-[#2A2A2A] focus:outline-none text-white"
-            />
-          </div>
-          <!-- confirm password -->
-          <div
-            class="md:w-[18rem] w-[16rem] bg-[#2A2A2A] border border-white rounded-[5px] mx-auto mt-7 flex py-1.5 px-4 space-x-5 justify-center items-center"
-          >
-          <ion-icon name="lock-closed" class="text-[#A19393]"></ion-icon>
-          <input
-              type="password"
-              name=""
-              id="password"
-              placeholder="Confirm Password"
-              class="text-sm bg-[#2A2A2A] focus:outline-none text-white"
-            />
-          </div>
-          <!-- login -->
-          <div
-            class="md:w-[18rem] w-[16rem] bg-blue-500 rounded-[8px] mx-auto mt-8 flex py-2 justify-center items-center"
-          >
-            <p class="text-[16px] text-white font-semibold">REGISTER NOW</p>
-          </div>
-          <p class="mt-10 text-center text-white text-sm underline font-semibold">Privacy Policy</p>
-          <p class="mt-4 text-center text-white text-[13px]">Already have an account?<span class="underline text-[12px]">  Sign In</span></p>
-        </div>
-      </div>
     </div>
 </body>
 
