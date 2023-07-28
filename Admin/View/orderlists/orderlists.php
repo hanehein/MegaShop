cd787fedf59c481997be3e29d8a4375c2274050c
Drey <bhon3myatpaing@gmail.com>
2023-07-28 19:39:37 +0630

    mobile view updates

--- Admin/View/orderlists/orderlists.php
+++ Admin/View/orderlists/orderlists.php
@@ -160,36 +160,16 @@
         <div class="w-full flex flex-col  items-center justify-start bg-gray-200 space-y-8 font-['Poppins'] ">
             <div class="w-full flex items-center justify-center h-20 bg-[#00336659] space-x-80">
                 Order by
-                <div class="relative inline-block text-left">
-                    <div>
-                        <button type="button" class="inline-flex w-full justify-center  ml-5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm  hover:bg-gray-50" id="menu-button">
-                            Options
-                            <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
-                    </div>
-
-                    <!--
-    Dropdown menu, show/hide based on menu state.
-
-    Entering: "transition ease-out duration-100"
-      From: "transform opacity-0 scale-95"
-      To: "transform opacity-100 scale-100"
-    Leaving: "transition ease-in duration-75"
-      From: "transform opacity-100 scale-100"
-      To: "transform opacity-0 scale-95"
-  -->
-                    <div class="absolute hidden right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg  focus:outline-none" role="menu">
-                        <div class="py-1" role="none">
-                            <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                            <a href="#" class="text-gray-700 block px-4 py-2 text-sm" id="menu-item-0">Order Id</a>
-                            <a href="#" class="text-gray-700 block px-4 py-2 text-sm"  id="menu-item-1">Price</a>
-                            <a href="#" class="text-gray-700 block px-4 py-2 text-sm"  id="menu-item-2">License</a>
-                            
-                        </div>
-                    </div>
-                </div>
+                <select name="" id="" class="text-black px-5 font-semibold rounded-lg py-2">
+                    <option value="">Order ID</option>
+                    <option value="">Product Name</option>
+                    <option value="">Qty</option>
+                    <option value="">Price Per Item</option>
+                    <option value="">Discount</option>
+                    <option value="">Date</option>
+                    <option value="">Total Price</option>
+                    
+                </select>
                 <div class="w-32 h-8 flex items-center justify-center bg-white text-[#003366] text-xs rounded-md font-semibold hover:text-white hover:bg-[#003366]">
                     <input type="text" name="" id="" class="text-xs bg-white text-[#003366] text-xs rounded-md font-semibold hover:text-white" placeholder="Search by ID Number ">
 
@@ -221,7 +201,7 @@
                             <td>COD</td>
                             <td>42,750 MMK</td>
 
-                            <td><a href=""><button class="w-16 py-1 rounded-md bg-red-600 text-white text-xs hover:text-red-600 hover:bg-gray-700">Delete</button></a></td>
+                            
                         </tr>
                         <tr class="h-14 border-b-2 border-b-white hover:bg-[#00336618]">
                             <td>#112233</td>
@@ -233,7 +213,7 @@
                             <td>COD</td>
                             <td>42,750 MMK</td>
 
-                            <td><a href=""><button class="w-16 py-1 rounded-md bg-red-600 text-white text-xs hover:text-red-600 hover:bg-gray-700">Delete</button></a></td>
+                           
                         </tr>
                         <tr class="h-14 border-b-2 border-b-white hover:bg-[#00336618]">
                             <td>#112233</td>
@@ -245,7 +225,7 @@
                             <td>COD</td>
                             <td>42,750 MMK</td>
 
-                            <td><a href=""><button class="w-16 py-1 rounded-md bg-red-600 text-white text-xs hover:text-red-600 hover:bg-gray-700">Delete</button></a></td>
+                          
                         </tr>
                         <tr class="h-14 border-b-2 border-b-white hover:bg-[#00336618]">
                             <td>#112233</td>
@@ -257,7 +237,7 @@
                             <td>COD</td>
                             <td>42,750 MMK</td>
 
-                            <td><a href=""><button class="w-16 py-1 rounded-md bg-red-600 text-white text-xs hover:text-red-600 hover:bg-gray-700">Delete</button></a></td>
+                           
                         </tr>
                         <tr class="h-14 border-b-2 border-b-white hover:bg-[#00336618]">
                             <td>#112233</td>
@@ -269,7 +249,7 @@
                             <td>COD</td>
                             <td>42,750 MMK</td>
 
-                            <td><a href=""><button class="w-16 py-1 rounded-md bg-red-600 text-white text-xs hover:text-red-600 hover:bg-gray-700">Delete</button></a></td>
+                           
                         </tr>
                         <tr class="h-14 border-b-2 border-b-white hover:bg-[#00336618]">
                             <td>#112233</td>
@@ -281,7 +261,7 @@
                             <td>COD</td>
                             <td>42,750 MMK</td>
 
-                            <td><a href=""><button class="w-16 py-1 rounded-md bg-red-600 text-white text-xs hover:text-red-600 hover:bg-gray-700">Delete</button></a></td>
+                           
                         </tr>
                         <tr class="h-14 border-b-2 border-b-white hover:bg-[#00336618]">
                             <td>#112233</td>
@@ -293,7 +273,7 @@
                             <td>COD</td>
                             <td>42,750 MMK</td>
 
-                            <td><a href=""><button class="w-16 py-1 rounded-md bg-red-600 text-white text-xs hover:text-red-600 hover:bg-gray-700">Delete</button></a></td>
+                           
                         </tr>
 
                     </tbody>
