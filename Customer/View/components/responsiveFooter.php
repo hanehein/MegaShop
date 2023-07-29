f9bcc3873a07a7e8d36a474f3567fb6d1f31d01c
Drey <bhon3myatpaing@gmail.com>
2023-07-29 15:10:49 +0630

    fix file names + components

---
+++ Customer/View/components/responsiveFooter.php
@@ -0,0 +1,33 @@
+<div class="w-full  flex flex-col items-center justify-center font-['Poppins'] bg-[#024486] mt-auto">
+        <!-- first one -->
+        <div class="md:w-full w-[28rem] container md:flex mx-auto  items-center justify-between sm:justify-center mt-3 py-3 md:space-x-20">
+            <!-- quicklink -->
+            <div class=" md:w-50 w-[28rem] flex flex-col  items-center justify-center text-white text-sm font-semibold px-3 py-2">
+                <p>Quick Links</p>
+                <ul class="list-disc mt-2">
+                    <li>About Us</li>
+                    <li>Pricacy & Policy</li>
+                </ul>
+            </div>
+            <!-- Help -->
+            <div class=" md:w-50 w-[28rem] flex flex-col  items-center justify-center text-white text-sm font-semibold px-3 py-2 md:border-hidden border-t-2  border-b-white">
+                <p>Help?</p>
+                <ul class="list-disc mt-2">
+                    <li>Payment</li>
+                    <li>FAQ</li>
+                </ul>
+            </div>
+            <!-- Contact Us -->
+            <div class=" w-50 md:w-[28rem] flex flex-col items-center justify-center text-white text-sm font-semibold px-3 py-2 md:border-hidden border-t-2 border-b-white ">
+                <p>Contact Us</p>
+                <ul class="list-disc mt-2 leading-5">
+                    <li><ion-icon name="phone-portrait"></ion-icon><span class="ml-2">+95777777777</span></li>
+                    <li><ion-icon name="mail"></ion-icon><span class="ml-2">service@gmail.com</span></li>
+                </ul>
+            </div>
+        </div>
+        <!-- last one -->
+        <div class="md:w-[80rem] w-[28rem] flex items-center justify-center border-t-2 border-b-white mt-1 py-3">
+            <p class="text-white text-sm font-semibold">&#64;2023 MEGA SHOP All Rights Reserved.</p>
+        </div>
+    </div>
--- Customer/View/profile/myReviews.php
+++ Customer/View/profile/myReviews.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order History</title>
+    <title>My Reviews</title>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <link rel="stylesheet" href="../resources/lib/tailwind/output.css?id=<?= time() ?>">
@@ -27,7 +27,7 @@
     </style>
 </head>
 
-<body class="bg-[#F6F6F6] font-['Poppins] no-scrollbar flex flex-col min-h-screen">
+<body class="bg-[#F6F6F6] font-['Poppins] no-scrollbar flex flex-col min-h-screen ">
     <div class="w-full flex flex-col items-center">
         <!-- navbar -->
         <?php include '../components/responsiveNav.php';?>
@@ -45,9 +45,9 @@
 
             <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Followed Stores</p>
 
-            <p class="text-md py-3 font-semibold cursor-pointer hover:font-semibold">Order History</p>
+            <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Order History</p>
 
-            <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">My Reviews</p>
+            <p class="text-md py-3 font-semibold cursor-pointer hover:font-semibold">My Reviews</p>
 
             <p class="text-md py-3 font-light cursor-pointer hover:font-semibold">Wishlist</p>
 
@@ -69,55 +69,88 @@
                 <p class="text-xs ml-2 mr-2 font-semibold border-b-2 border-transparent px-1 py-1 hover:border-black">Wishlist</p>
             </div>
         </div>
-        <!-- history -->
+        <!-- reviews -->
         <div class="w-full md:w-1/2 items-center flex mx-auto mt-20">
             <div class="flex flex-col items-center justify-center">
                 <p class="text-xl font-semibold">
                     Order History
                 </p>
                 <div class="rounded-lg  bg-[#FFFFFF] shadow-2xl mb-3 mt-3 border-solid border-2">
-                    <div class="bg-[#003366] text-white text-lg md:text-xl font-semibold rounded-tr-lg rounded-tl-lg px-5 py-3">
+                    <div class="bg-[#003366] text-white text-md md:text-xl font-semibold rounded-tr-lg rounded-tl-lg px-5 py-3">
                         Traffic Star >>>
                     </div>
                     <div class="flex items-center justify-between">
-                        <img src="../resources/img/backpack 1.jpg" alt="" class="w-1/3 md:w-1/6 h-1/3 ml-10 mt-3 rounded-lg mb-3">
-                        <div class="py-3 text-left px-5 mr-10 ml-5 md:mr-20">
+                        <img src="../resources/img/backpack 1.jpg" alt="" class="w-1/4 md:w-1/6 h-1/3 ml-10 mt-3 rounded-lg mb-3">
+                        <div class="py-3 text-left px-2 md:px-5 mr-10 ml-5 md:mr-20">
                             <p class="font-semibold text-sm md:text-lg mb-2 mt-2"> Tucano Backpacks TU-BP01</p>
                             <p class="mb-2 mt-2 text-xs">Tucano, Color Family:Blue</p>
                             <p class="mb-2 mt-2 text-xs">Color :Blue</p>
                             <p class="mb-2 mt-2 text-xs">Size : ###</p>
                         </div>
+                        <div class="flex flex-col items-center mr-5 md:mr-10">
+                            <p class="text-md md:text-xl font-semibold mt-5 mb-5">5.0</p>
+                            <div class="text-[#facc15] mt-5 mb-5 flex justify-evenly px-1">
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
                 <div class="rounded-lg  bg-[#FFFFFF] shadow-2xl mb-3 mt-3 border-solid border-2">
-                    <div class="bg-[#003366] text-white text-lg md:text-xl font-semibold rounded-tr-lg rounded-tl-lg px-5 py-3">
+                    <div class="bg-[#003366] text-white text-md md:text-xl font-semibold rounded-tr-lg rounded-tl-lg px-5 py-3">
                         Traffic Star >>>
                     </div>
                     <div class="flex items-center justify-between">
-                        <img src="../resources/img/backpack 1.jpg" alt="" class="w-1/3 md:w-1/6 h-1/3 ml-10 mt-3 rounded-lg mb-3">
-                        <div class="py-3 text-left px-5 mr-10 ml-5 md:mr-20">
+                        <img src="../resources/img/backpack 1.jpg" alt="" class="w-1/4 md:w-1/6 h-1/3 ml-10 mt-3 rounded-lg mb-3">
+                        <div class="py-3 text-left px-2 md:px-5 mr-10 ml-5 md:mr-20">
                             <p class="font-semibold text-sm md:text-lg mb-2 mt-2"> Tucano Backpacks TU-BP01</p>
                             <p class="mb-2 mt-2 text-xs">Tucano, Color Family:Blue</p>
                             <p class="mb-2 mt-2 text-xs">Color :Blue</p>
                             <p class="mb-2 mt-2 text-xs">Size : ###</p>
                         </div>
+                        <div class="flex flex-col items-center mr-5 md:mr-10">
+                            <p class="text-md md:text-xl font-semibold mt-5 mb-5">5.0</p>
+                            <div class="text-[#facc15] mt-5 mb-5 flex justify-evenly px-1">
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
                 <div class="rounded-lg  bg-[#FFFFFF] shadow-2xl mb-3 mt-3 border-solid border-2">
-                    <div class="bg-[#003366] text-white text-lg md:text-xl font-semibold rounded-tr-lg rounded-tl-lg px-5 py-3">
+                    <div class="bg-[#003366] text-white text-md md:text-xl font-semibold rounded-tr-lg rounded-tl-lg px-5 py-3">
                         Traffic Star >>>
                     </div>
                     <div class="flex items-center justify-between">
-                        <img src="../resources/img/backpack 1.jpg" alt="" class="w-1/3 md:w-1/6 h-1/3 ml-10 mt-3 rounded-lg mb-3">
-                        <div class="py-3 text-left px-5 mr-10 ml-5 md:mr-20">
+                        <img src="../resources/img/backpack 1.jpg" alt="" class="w-1/4 md:w-1/6 h-1/3 ml-10 mt-3 rounded-lg mb-3">
+                        <div class="py-3 text-left px-2 md:px-5 mr-10 ml-5 md:mr-20">
                             <p class="font-semibold text-sm md:text-lg mb-2 mt-2"> Tucano Backpacks TU-BP01</p>
                             <p class="mb-2 mt-2 text-xs">Tucano, Color Family:Blue</p>
                             <p class="mb-2 mt-2 text-xs">Color :Blue</p>
                             <p class="mb-2 mt-2 text-xs">Size : ###</p>
                         </div>
+                        <div class="flex flex-col items-center mr-5 md:mr-10">
+                            <p class="text-md md:text-xl font-semibold mt-5 mb-5">5.0</p>
+                            <div class="text-[#facc15] mt-5 mb-5 flex justify-evenly px-1">
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                                <ion-icon name="star"></ion-icon>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
-                
+
             </div>
         </div>
 
