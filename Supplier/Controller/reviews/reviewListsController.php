da73828ddd229191347c4637b34d3ec6596ddc1d
Maung Thar Kyaw <maungtharkyaw2520@gmail.com>
2023-08-05 00:22:52 +0630

    rating and review supplier backend

---
+++ Supplier/Controller/reviews/reviewListsController.php
@@ -0,0 +1,23 @@
+<?php
+
+//connect database
+include "../../Model/model.php";
+
+$sql = $pdo->prepare(
+    "SELECT 
+    t_product_reviews.rating AS rating, 
+    t_product_reviews.review AS review,
+    m_products.p_name AS p_name, 
+    m_customers.cus_name AS cus_name, 
+    t_product_reviews.create_date AS create_date,
+    m_products.id AS p_id
+    FROM t_product_reviews
+    INNER JOIN m_products ON t_product_reviews.product_id = m_products.id
+    INNER JOIN m_customers ON t_product_reviews.customer_id = m_customers.id
+    WHERE t_product_reviews.del_flg = 0;
+    "
+);
+$sql->execute();
+$reviewLists = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+?>
