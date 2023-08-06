535edcd316ac14438f6e7c599cd4b87db9f3ee3e
DESKTOP-LOPUSIN\User <szonebylwin98@gmail.com>
2023-08-06 22:12:39 +0630

    sup_account(login,signup,OTP 3pages)

---
+++ Supplier/View/supaccount/supOTP2.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Supplier OTP 2</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body>
+    <section class="w-screen bg-gray-50 min-h-screen flex items-center justify-center">
+        <div class=" bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5">
+            <!-- photo container -->
+            <div class="w-1/2 py-10 px-10 md:flex flex-col hidden items-center justify-center">
+                <img class="rounded-[250px] w-[550px] h-[200px] shadow-xl"
+                    style="border-radius: 40px; box-shadow: 0 4px 15px 10px rgba(44, 118, 7, 0.5);"
+                    src="../img/verify.jpg">
+            </div>
+            <!-- input container -->
+            <div class="md:w-1/2 relative flex flex-col items-center justify-center">
+                <!-- "Back" tag -->
+                <div class="absolute top-0 right-0 mt-4 mr-4 md:mt-0 md:mr-8">
+                    <a href="https://www.google.com" class="underline" style="color: #F68721;">Back</a>
+                </div>
+                <div class="md:px-8 flex-col justify-center items-center">
+                    <h6 class="mt-14 text-center font-extrabold text-[18px] text-[#2C7607]">Verify E-mail Address</h6>
+                    <span class=" text-center font-medium text-[12px] text-[#2C7607]">Enter confirmation code that sent
+                        to associated with your account.</span>
+
+                </div>
+                <div class="flex justify-between m-5">
+                    <div class="w-[50px] h-[50px] bg-green-200 flex items-center justify-center border border-[#2C7607] shadow-md"
+                        style="margin-right: 10px;">4</div>
+                    <div class="w-[50px] h-[50px] bg-green-200 flex items-center justify-center border border-[#2C7607] shadow-md"
+                        style="margin-right: 10px;">5</div>
+                    <div class="w-[50px] h-[50px] bg-green-200 flex items-center justify-center border border-[#2C7607] shadow-md"
+                        style="margin-right: 10px;">6</div>
+                    <div class="w-[50px] h-[50px] bg-green-200 flex items-center justify-center border border-[#2C7607] shadow-md"
+                        style="margin-right: 10px;">7</div>
+                </div>
+                <button
+                    class="my-4 bg-[#2C7607] rounded-xl text-white text-base py-4 px-12 hover:scale-105 duration-300">Confirm
+                    mail</button>
+            </div>
+        </div>
+    </section>
+    </body>
+
+</html>
+<?php $_SESSION["loginerror"] = "" ?>
---
+++ Supplier/View/supaccount/supSignup.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Supplier Login</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body>
+    <!-- signUp container -->
+    <div class="min-h-screen flex items-center justify-center container">
+        <!-- bg & photo container -->
+        <div class="h-screen md:flex flex-col hidden w-1/2 bg-[#EFFFE8] items-center justify-center shadow-xl rounded-tr-[80px] rounded-br-[80px]" style="box-shadow: 0 4px 50px #2C760786;">
+            <h3 class="text-[30px] text-center font-extrabold italic text-[#2C7607] pt-7">Welcome to</h3>
+            <h1 class="text-[50px] text-center font-extrabold text-[#2C7607] mt-5 tracking-[10px]">MEGA-SHOP</h1>
+            <div class="relative w-2/3 rounded-[100px] overflow-hidden m-10 place-content-center">
+                <img src="../supImg/s1.jpg" alt="" class="w-full h-full object-cover">
+                <div class="absolute inset-0 shadow-inner" style="border-radius: 40px; box-shadow: inset 0 4px 15px 10px rgba(0, 0, 0, 0.2);"></div>
+            </div>
+            <span class="text-[#2C7607] text-[20px] text-center">2023@ MEGA-SHOP All rights reserved.</span>
+        </div>
+        
+        <!-- input container -->
+        <div class="w-1/2 px-8 md:px-16 flex flex-col justify-center items-center">
+            <h4 class="text-center font-extrabold text-[25px] text-[#2C7607]">Sign Up</h4>
+            <form action="" class="flex flex-col gap-4">
+                <input class="p-2 my-2 rounded-xl border border-[#2C7607] shadow-md w-[400px] h-[60px] relative" type="text" name="name" placeholder="Name">
+                <input class="p-2 my-2 rounded-xl border border-[#2C7607] shadow-md w-[400px] h-[60px] relative" type="email" name="email" placeholder="Email">
+                <input class="p-2 my-2 rounded-xl border border-[#2C7607] shadow-md w-[400px] h-[60px] relative" type="tel" name="phone" placeholder="Phone number">
+                <div class="relative">
+                    <input class="p-2 my-2 rounded-xl border border-[#2C7607] shadow-md w-[400px] h-[60px]" type="password" name="password" placeholder="Password">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#2C7607"
+                        class="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer text-[#2C7607] transform group-hover:text-[#2C7607]"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path
+                            d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                    </svg>
+                </div>
+                <div class="relative">
+                    <input class="p-2 my-2 rounded-xl border border-[#2C7607] shadow-md w-[400px] h-[60px]" type="password" name="confirm_password" placeholder="Confirm Password">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#2C7607"
+                        class="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer text-[#2C7607] transform group-hover:text-[#2C7607]"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path
+                            d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                    </svg>
+                </div>
+                <div class="flex items-center">
+                    <input type="checkbox" class="form-checkbox h-5 w-5 text-[#2C7607]">
+                    <label for="privacy_policy" class="ml-2">
+                        I agree with <span class="text-red-500">Privacy</span> and <span class="text-red-500">Policy</span>
+                    </label>
+                </div>
+                <button class="my-4 bg-[#2C7607] rounded-xl text-white py-2 hover:scale-105 duration-300">Sign Up</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
+<?php $_SESSION["loginerror"] = "" ?>
