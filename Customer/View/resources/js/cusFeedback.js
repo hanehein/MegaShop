07d42bc411a20ebd86ca2da75be7334fdd56c41f
DESKTOP-GM29KLB\HP <myatnoe.khin.9440@gmail.com>
2023-08-15 21:28:32 +0630

    fix error

--- Customer/View/resources/js/cusFeedback.js
+++ Customer/View/resources/js/cusFeedback.js
@@ -3,11 +3,7 @@ const rateBtns = document.querySelectorAll(".rate");
 const message = document.getElementById("message");
 // message.innerText ="";
 //     rateVal.value = ""
-rateBtns.forEach(rateBtn => {
-  console.log(rateBtn);
-  rateBtn.addEventListener("click", function (e){
-    e.preventDefault(); 
-    rateVal.value= e.value;
+
 
 rateBtns.forEach( rateBtn => {
   rateBtn.addEventListener('click', function(e){
@@ -24,7 +20,7 @@ function removeActive(){
   rateBtns.forEach(btn => {
     btn.classList.remove("active");
   })
-});
+};
 
 
 
