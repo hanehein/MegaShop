eb557f131a26ad0ac5779ca920cdb464bd71c8f6
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2023-08-19 23:01:06 +0630

    last minutes

--- Customer/Controller/shopListController.php
+++ Customer/Controller/shopListController.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 
 include "../../Model/model.php";
 $sql = $pdo->prepare("
-    SELECT m_suppliers.id as supplierId, sup_shop_name as shopName, shop_photo_path as shopPhoto
+    SELECT *, m_suppliers.id as supplierId, sup_shop_name as shopName, shop_photo_path as shopPhoto
     FROM m_suppliers
     WHERE del_flg = 0
     AND sup_approve = 1
@@ -15,7 +15,7 @@ $sql->execute();
 $shopLists = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 $sql2 = $pdo->prepare("
-    SELECT m_suppliers.id as supplierId, sup_shop_name as shopName, shop_photo_path as shopPhoto
+    SELECT *, m_suppliers.id as supplierId, sup_shop_name as shopName, shop_photo_path as shopPhoto
     FROM m_suppliers
     WHERE del_flg = 0
     AND sup_approve = 1
