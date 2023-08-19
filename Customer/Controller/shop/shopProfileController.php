e8bd97ae48f43b4ba8026fdb0c630363a321fd83
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2023-08-19 14:46:20 +0630

    update process

--- Customer/Controller/shop/shopProfileController.php
+++ Customer/Controller/shop/shopProfileController.php
@@ -1,8 +1,14 @@
 <?php
 ini_set('display_errors', 1);
 //DB connection
-include "../../Model/model.php";
-$sql = $pdo->prepare(
+$id = $_GET["id"];
+// print_r($id);
+
+if (!isset($id)) {
+  header("Location: ../View/errors/error.php");
+} else {
+  include "../../Model/model.php";
+  $sql = $pdo->prepare(
     "SELECT * FROM m_products 
     INNER JOIN t_product_reviews 
     ON m_products.id = t_product_reviews.product_id"
@@ -15,9 +21,10 @@ $sql = $pdo->prepare(
   );
   $sql->execute();
   $products = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
+
 
 
   // echo "<pre>";
   // print_r($product_ratings);
 // echo "hi";
-?>
