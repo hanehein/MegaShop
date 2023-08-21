e71147e4f945e1e92c530f41331ddf16bbe3c40c
Maung Thar Kyaw <maungtharkyaw2520@gmail.com>
2023-08-22 05:12:01 +0630

    shopping cart update

--- Customer/View/order/shoppingCart.php
+++ Customer/View/order/shoppingCart.php
@@ -1,19 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include "../../Controller/shoppingcart/productListsController1.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>privacy&policy</title>
-    <link rel="stylesheet" href="../resources/lib/tailwind/output.css">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&family=Wallpoet&display=swap" rel="stylesheet">
-</head>
-
+<!-- start header -->
+<?php 
+$hasCssFile = FALSE;
+$hasJsFile = FALSE;
+include "./../components/header.php";
+?>
+<!-- end header -->
 <body>
     <!-- START NAVBAR -->
     
@@ -24,25 +19,77 @@
         <div class="container mx-auto block sm:grid sm:grid-cols-3 sm:gap-4">
             <div class="card-container sm:col-span-2 mb-4">
 
-                <!-- start cart -->
-                <div class="rounded-md overflow-hidden shadow-lg">
-                    <div class="p-2 bg-custom-blue">
-                        <p class="text-white"><a href="">Traffic Star</a></p>
-                    </div>
-                    <div class="flex justify-between p-4 items-center">
-                        <div class="block sm:hidden">
-                            <img class="max-h-[130px]" src="https://cdn.ospreyeurope.com/shop/media/catalog/product/cache/90fbb2341f4f0519805a950f9933da0d/_/0/_0035_ozonelaptopbackpack28_f22_side_coastalblue_10004643.jpg" alt="product-img">
+                <?php foreach($productLists as $product): ?>
+                    <!-- start cart -->
+                    <div class="rounded-md overflow-hidden shadow-lg mb-5">
+                        <div class="p-2 bg-custom-blue">
+                            <p class="text-white"><a href=""><?= $product["sup_shop_name"] ?></a></p>
                         </div>
-                        <div class="block sm:flex sm:justify-between sm:w-full sm:items-center">
-                            <div class="mb-2 flex items-center">
-                                <div class="hidden sm:block">
-                                    <img class="max-h-[100px]" src="https://cdn.ospreyeurope.com/shop/media/catalog/product/cache/90fbb2341f4f0519805a950f9933da0d/_/0/_0035_ozonelaptopbackpack28_f22_side_coastalblue_10004643.jpg" alt="product-img">
+                        <div class="flex justify-between p-4 items-center">
+                            <div class="block sm:hidden">
+                                <img class="max-w-[100px]" src="../../../<?= $product["p_photo1"] ?>" alt="product-img">
+                            </div>
+                            <div class="block sm:flex sm:justify-between sm:w-full sm:items-center">
+                                <div class="mb-2 flex items-center space-x-2">
+                                    <div class="hidden sm:block">
+                                        <img class="max-w-[100px]" src="../../../<?= $product["p_photo1"] ?>" alt="product-img">
+                                    </div>
+                                    <div>
+                                        <h2 class="text-custom-large font-bold">
+                                            <?php if (strlen($product["p_name"]) > 14) {
+                                                echo substr($product["p_name"], 0, 14) . "...";
+                                            } else {
+                                                echo $product["p_name"];
+                                            }
+                                            ?>
+                                        </h2>
+                                        
+                                        <p class="text-custom-grey">Color : blue</p>
+                                        <p class="text-custom-grey">Size : M</p>
+                                        <div class="space-x-2 hidden sm:flex">
+                                            <div class="items-center">
+                                                <ion-icon class="text-custom-medium" name="trash-bin"></ion-icon>
+                                                <span class="text-custom-tiny text-custom-grey"> Remove</span>
+                                            </div>
+                                            <div class="items-center">
+                                                <ion-icon class="text-custom-medium" name="heart-outline"></ion-icon>
+                                                <span class="text-custom-tiny text-custom-grey"> Add To Wishlists</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex space-x-2 sm:flex-col items-center mb-2">
+                                    <?php 
+                                        $discountPrice = ( $product["p_discount"] * $product["p_sell_price"] ) / 100;
+                                        $price = $product["p_sell_price"] - $discountPrice;
+                                    ?>
+                                    <?php if($product["p_discount"] != 0): ?>
+                                        <p class="text-custom-orange text-custom-extra-large"><?= $price ?> Ks</p>
+                                        <p class="text-custom-tiny line-through"><?= $product["p_sell_price"] ?> Ks</p>
+                                        <p class="text-custom-tiny text-custom-orange">(<?= $product["p_discount"] ?>%)</p>
+                                    <?php else: ?>
+                                        <p class="text-custom-orange text-custom-extra-large"><?= $price ?> Ks</p>
+                                    <?php endif; ?>
+                                    
                                 </div>
                                 <div>
-                                    <h2 class="text-custom-large font-bold">Tucano Backpacks TU-BP01</h2>
-                                    <p class="text-custom-grey">Color : blue</p>
-                                    <p class="text-custom-grey">Size : M</p>
-                                    <div class="space-x-2 hidden sm:flex">
+                                    <div class="items-center">
+                                        <div class="flex space-x-2 items-center">
+                                            <div class="w-[30px] h-[30px] rounded-md bg-custom-grey flex justify-center items-center">
+                                                <ion-icon class="text-white" name="remove"></ion-icon>
+                                            </div>
+                                            <div class="w-[50px] h-[30px] rounded-md bg-grey flex justify-center items-center bg-transparent">
+                                                <span class="font-bold">1</span>
+                                            </div>
+                                            <div class="w-[30px] h-[30px] rounded-md bg-custom-grey flex justify-center items-center">
+                                                <ion-icon class="text-white" name="add"></ion-icon>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <small class="font-custom-tiny">only 12 items left</small>
+                                        </div>
+                                    </div>
+                                    <div class="space-x-2 flex sm:hidden">
                                         <div class="items-center">
                                             <ion-icon class="text-custom-medium" name="trash-bin"></ion-icon>
                                             <span class="text-custom-tiny text-custom-grey"> Remove</span>
@@ -54,43 +101,10 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="flex space-x-2 sm:flex-col items-center mb-2">
-                                <p class="text-custom-orange text-custom-extra-large">Ks 23450</p>
-                                <p class="text-custom-tiny line-through">Ks 33500</p>
-                                <p class="text-custom-tiny text-custom-orange">(30%)</p>
-                            </div>
-                            <div>
-                                <div class="items-center">
-                                    <div class="flex space-x-2 items-center">
-                                        <div class="w-[30px] h-[30px] rounded-md bg-custom-grey flex justify-center items-center">
-                                            <ion-icon class="text-white" name="remove"></ion-icon>
-                                        </div>
-                                        <div class="w-[50px] h-[30px] rounded-md bg-grey flex justify-center items-center bg-transparent">
-                                            <span class="font-bold">1</span>
-                                        </div>
-                                        <div class="w-[30px] h-[30px] rounded-md bg-custom-grey flex justify-center items-center">
-                                            <ion-icon class="text-white" name="add"></ion-icon>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <small class="font-custom-tiny">only 12 items left</small>
-                                    </div>
-                                </div>
-                                <div class="space-x-2 flex sm:hidden">
-                                    <div class="items-center">
-                                        <ion-icon class="text-custom-medium" name="trash-bin"></ion-icon>
-                                        <span class="text-custom-tiny text-custom-grey"> Remove</span>
-                                    </div>
-                                    <div class="items-center">
-                                        <ion-icon class="text-custom-medium" name="heart-outline"></ion-icon>
-                                        <span class="text-custom-tiny text-custom-grey"> Add To Wishlists</span>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
-                </div>
-                <!-- end cart -->
+                    <!-- end cart -->
+                <?php endforeach; ?>
 
             </div>
 
@@ -98,11 +112,23 @@
                 <div class="order-container bg-custom-blue text-white rounded-md p-4">
                     <h2 class="text-custom-extra-large font-bold">Total</h2>
                     <!-- start itemlists -->
+                    <?php $totalPrice = 0; ?>
                     <div class="itemlists-container mb-4">
-                        <div class="item flex justify-between">
-                            <span>item1</span>
-                            <span>23450 Ks</span>
-                        </div>
+                        <?php foreach($productLists as $product): ?>
+                            <div class="item flex justify-between mb-2">
+                                <?php if (strlen($product["p_name"]) > 20): ?>
+                                    <span><?= substr($product["p_name"], 0, 20) . "..." ?></span>
+                                <?php else: ?>
+                                    <span><?= $product["p_name"] ?></span>
+                                <?php endif; ?>
+                                <?php
+                                    $discountPrice = ( $product["p_discount"] * $product["p_sell_price"] ) / 100;
+                                    $price = ($product["p_sell_price"] - $discountPrice) * $product["qty"];
+                                    $totalPrice = $totalPrice + $price;
+                                ?>
+                                <span><?= $price ?> Ks</span>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
                     <!-- end itemlists -->
 
@@ -110,7 +136,7 @@
 
                     <div class="total flex justify-between font-bold mb-4">
                         <span>Total</span>
-                        <span>23450 Ks</span>
+                        <span><?= $totalPrice ?> Ks</span>
                     </div>
 
                     <button class="checkout-btn bg-custom-orange w-full rounded-md text-white text-custom-extra-large font-bold uppercase py-1">
