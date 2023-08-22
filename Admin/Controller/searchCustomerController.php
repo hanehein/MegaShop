52e08eee537a34a361000a5e7c934b026b07455d
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2023-08-22 22:29:09 +0630

    final update

--- Admin/Controller/searchCustomerController.php
+++ Admin/Controller/searchCustomerController.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 1);
     $searchName = $_POST["searchText"];
     
     $sql = $pdo->prepare("
-        SELECT * FROM m_customers WHERE del_flg = 0 AND cus_registered = 1 AND cus_name LIKE :search
+        SELECT * FROM m_customers WHERE del_flg = 0 AND cus_name LIKE :search
     ");
     $sql->bindValue(":search",'%'.$searchName.'%');
     $sql->execute();
--- Admin/View/resources/js/searchCustomer.js
+++ Admin/View/resources/js/searchCustomer.js
@@ -11,44 +11,7 @@ $(document).ready(function () {
         let customers = JSON.parse(res);
         console.log(customers);
         for (const customer of customers) {
-          //township
-          let region = "";
-
-          if (customer.region_id == 1) {
-            region = "Kachin State";
-          } else if (customer.region_id == 2) {
-            region = "Kayah State";
-          } else if (customer.region_id == 3) {
-            region = "Kayin State";
-          } else if (customer.region_id == 4) {
-            region = "Chin State";
-          } else if (customer.region_id == 5) {
-            region = "Sagaing Region";
-          } else if (customer.region_id == 6) {
-            region = "Tanintharyi Region";
-          } else if (customer.region_id == 7) {
-            region = "Bago Region";
-          } else if (customer.region_id == 8) {
-            region = "Mon State";
-          } else if (customer.region_id == 9) {
-            region = "Magway Region";
-          } else if (customer.region_id == 10) {
-            region = "Mandalay Region";
-          } else if (customer.region_id == 11) {
-            region = "Shan State";
-          } else if (customer.region_id == 12) {
-            region = "Yangon Region";
-          } else if (customer.region_id == 13) {
-            region = "Rakhine State";
-          } else if (customer.region_id == 14) {
-            region = "Ayeyarwady Region";
-          } else {
-            region = "Naypyidaw Union Territory";
-          }
-          //gender 
-          let gender = "";
-          if(customer.gender == 0) {gender = "Male";}
-          else {gender = "Female";}
+         
           $("#searchResult").append(
             `
                             <tr class="h-10 border-b-2 border-b-white hover:bg-[#00336618]">
@@ -57,8 +20,6 @@ $(document).ready(function () {
                                 <td>${customer.cus_name}</td>
                                 <td>${customer.cus_email}</td>
                                 <td>${customer.cus_phone}</td>
-                                <td>${gender}</td>
-                                <td>${region}</td>
                                 <td>${customer.cus_address}</td>
                                 <td>${customer.create_date}</td>
                                 <td><a href="../../Controller/customerNotifyController.php?id=${customer.id}"><button class="w-16 py-1 rounded-md bg-[#003366] text-white text-xs hover:text-white hover:bg-[#66CC33]">Notify</button></a></td>
