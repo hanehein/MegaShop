39f6f18525a0add167273560156eea7ec229a148
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2023-08-31 11:29:13 +0630

    update model

--- Admin/Model/model.php
+++ Admin/Model/model.php
@@ -3,11 +3,12 @@
 $hostname = "mega-shop-do-user-14537762-0.b.db.ondigitalocean.com";
 $port = 25060;
 $dbname = "megashop_db";
-$uname = "root";
+$uname = "doadmin";
 $pwd = "AVNS_Qr_6jGlFpoH7Mj_qaj_";
 
 //Connection
-$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$dbname",
+$pdo = new PDO(
+    "mysql:host=$hostname;port=$port;dbname=$dbname",
     $uname,
     $pwd
 );
--- Supplier/Model/model.php
+++ Supplier/Model/model.php
@@ -3,14 +3,12 @@
 $hostname = "mega-shop-do-user-14537762-0.b.db.ondigitalocean.com";
 $port = 25060;
 $dbname = "megashop_db";
-$uname = "root";
+$uname = "doadmin";
 $pwd = "AVNS_Qr_6jGlFpoH7Mj_qaj_";
-//connection
-$pdo = new PDO(
-    "mysql:hostname=$hostname; port =$port; dbname=$dbname",
+
+//Connection
+$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$dbname",
     $uname,
     $pwd);
 //set error exc
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-?>
